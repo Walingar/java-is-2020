c71d9485afd7c1961db4a5f1c69ec30ab731362a
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2020-02-12 22:06:54 +0300

    Initialize repository

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    kotlin("jvm") version "1.3.60"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
