b3c794d213296fc0da945782f79b267606a8a778
anastasiiatitova <staceylemann@gmail.com>
2020-02-17 13:31:14 +0300

    fixups

--- src/main/java/impl/image/ImageConverterImpl.java
+++ src/main/java/impl/image/ImageConverterImpl.java
@@ -7,7 +7,10 @@ import java.awt.*;
 public class ImageConverterImpl implements ImageConverter {
     @Override
     public Color[][] convertToColor(int[][] image) {
-        Color[][] color = new Color[image.length][image[0].length];
+        Color[][] color = new Color[image.length][];
+        for (var i = 0; i < image.length; i++){
+            color[i] = new Color[image[i].length];
+        }
 
         for (var i = 0; i < image.length; i++) {
             for (var j = 0; j < image[i].length; j++) {
@@ -20,11 +23,14 @@ public class ImageConverterImpl implements ImageConverter {
 
     @Override
     public int[][] convertToRgb(Color[][] image) {
-        int[][] color = new int[image.length][image[0].length];
+        int[][] color = new int[image.length][];
+        for (var i = 0; i < image.length; i++){
+            color[i] = new int[image[i].length];
+        }
 
         for (int i = 0; i < image.length; i++) {
             for (int j = 0; j < image[i].length; j++) {
-                color[i][j] =image[i][j].getRGB();
+                color[i][j] = image[i][j].getRGB();
             }
         }
 
