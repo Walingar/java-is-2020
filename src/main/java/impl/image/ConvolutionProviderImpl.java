13d6b65c88eed3ae56daafa6d1675063d2784d71
anastasiiatitova <staceylemann@gmail.com>
2020-02-17 13:31:14 +0300

    fixups

--- src/main/java/impl/image/ConvolutionProviderImpl.java
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -7,37 +7,48 @@ import java.awt.*;
 public class ConvolutionProviderImpl implements ConvolutionProvider {
     @Override
     public Color[][] apply(Color[][] image, double[][] kernel) {
-        Color[][] image_copy = new Color[image.length][image[0].length];
+        Color[][] imageCopy = new Color[image.length][];
+        for (var i = 0; i < image.length; i++){
+            imageCopy[i] = new Color[image[i].length];
+        }
 
         for (var row = 0; row < image.length; row++) {
             for (var column = 0; column < image[row].length; column++) {
-                image_copy[row][column] = multiply(image, kernel, row, column);
+                imageCopy[row][column] = multiply(image, kernel, row, column);
             }
         }
-        return image_copy;
+        return imageCopy;
     }
 
     private Color multiply(Color[][] image, double[][] kernel, int row, int column) {
-        var radius = (kernel.length - 1) / 2;
-        int sumRed = 0, sumGreen = 0, sumBlue = 0;
+        int sumRed = 0;
+        int sumGreen = 0;
+        int sumBlue = 0;
 
         for (var i = 0; i < kernel.length; i++) {
             for (var j = 0; j < kernel[i].length; j++) {
-                var currentRow = row + i - radius;
-                var currentColumn = column + j - radius;
+                var radiusRow = (kernel.length - 1) / 2;
+                var radiusColumn = (kernel[i].length - 1) / 2;
+                var currentRow = row + i - radiusRow;
+                var currentColumn = column + j - radiusColumn;
                 if (areValidIndexes(currentRow, currentColumn, image)) {
-                    sumRed += image[currentRow][currentColumn].getRed() * kernel[i][j];
-                    sumGreen += image[currentRow][currentColumn].getGreen() * kernel[i][j];
-                    sumBlue += image[currentRow][currentColumn].getBlue() * kernel[i][j];
+                    var currentColor = image[currentRow][currentColumn];
+                    var currentKernelValue = kernel[i][j];
+                    sumRed += currentColor.getRed() * currentKernelValue;
+                    sumGreen += currentColor.getGreen() * currentKernelValue;
+                    sumBlue += currentColor.getBlue() * currentKernelValue;
                 }
             }
         }
 
-        return new Color(Math.min(sumRed, 255), Math.min(sumGreen, 255), Math.min(sumBlue, 255));
+        return new Color(getColorFromSum(sumRed), getColorFromSum(sumGreen), getColorFromSum(sumBlue));
     }
 
-    private boolean areValidIndexes(int row, int column, Color[][] image)
-    {
+    private boolean areValidIndexes(int row, int column, Color[][] image) {
         return  row >= 0 && row < image.length && column >= 0 && column < image[row].length;
+    }
+
+    private int getColorFromSum(int color) {
+        return Math.min(Math.max(0, color), 255);
     }
 }
