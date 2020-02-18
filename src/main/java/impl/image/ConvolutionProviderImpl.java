24dffc46b8ad4f28403f8eb0a651d035d436a736
Andrey Chausov <dxahtepb@gmail.com>
2020-02-18 04:11:24 +0300

    [task 1] ImageConvolutionProvider implemented

---
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -0,0 +1,90 @@
+package impl.image;
+
+import api.image.ConvolutionProvider;
+
+import java.awt.*;
+
+class ConvolutionProviderImpl implements ConvolutionProvider {
+
+    @Override
+    public Color[][] apply(Color[][] image, double[][] kernel) {
+        var imageHeight = image.length;
+        if (imageHeight == 0) {
+            return new Color[imageHeight][];
+        }
+        var imageWidth = image[0].length;
+
+        var wrappedImage = new ImageData(image, imageHeight, imageWidth);
+        var wrappedKernel = new KernelData(kernel, kernel.length / 2, kernel[0].length / 2);
+
+        var convertedImage = new Color[imageHeight][imageWidth];
+
+        for (var x = 0; x < wrappedImage.height; x++) {
+            for (var y = 0; y < wrappedImage.width; y++) {
+                convertedImage[x][y] = applyKernelToPixel(wrappedImage, wrappedKernel, x, y);
+            }
+        }
+
+        return convertedImage;
+    }
+
+    private static boolean isOutside(int length, int index) {
+        return index < 0 | index >= length;
+    }
+
+    private static Color applyKernelToPixel(ImageData image, KernelData kernel, int x, int y) {
+        var red = 0;
+        var green = 0;
+        var blue = 0;
+
+        for (var dx = -kernel.verticalRadius; dx <= kernel.verticalRadius; dx++) {
+            var shiftedX = x - dx;
+            if (isOutside(image.height, shiftedX)) {
+                continue;
+            }
+
+            var imageRow = image.image[shiftedX];
+            var kernelRow = kernel.kernel[dx + kernel.verticalRadius];
+
+            for (var dy = -kernel.horizontalRadius; dy <= kernel.horizontalRadius; dy++) {
+                var shiftedY = y - dy;
+                if (isOutside(image.width, shiftedY)) {
+                    continue;
+                }
+
+                var pixel = imageRow[shiftedY];
+                var kernelElement = kernelRow[dy + kernel.horizontalRadius];
+
+                red += pixel.getRed() * kernelElement;
+                green += pixel.getGreen() * kernelElement;
+                blue += pixel.getBlue() * kernelElement;
+            }
+        }
+
+       return new Color(red, green, blue);
+    }
+
+    private static class ImageData {
+        private final Color[][] image;
+        private final int height;
+        private final int width;
+
+        public ImageData(Color[][] image, int height, int width) {
+            this.image = image;
+            this.height = height;
+            this.width = width;
+        }
+    }
+
+    private static class KernelData {
+        private final double[][] kernel;
+        private final int verticalRadius;
+        private final int horizontalRadius;
+
+        public KernelData(double[][] kernel, int verticalRadius, int horizontalRadius) {
+            this.kernel = kernel;
+            this.verticalRadius = verticalRadius;
+            this.horizontalRadius = horizontalRadius;
+        }
+    }
+}
