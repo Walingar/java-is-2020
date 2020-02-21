81f8b53ca5c16a5af40da775654c41679c488f3b
Mike Saravayskiy <maiksaray@gmail.com>
2020-02-21 16:39:39 +0300

    addressed review comments:  * ConvolutionProviderImpl: removed comment, extracted reused variables and split declarations  * ImageConverterFactory: removed redundant empty line  * ImageConverterImpl: optimized imports, moved nested class

--- src/main/java/impl/image/ConvolutionProviderImpl.java
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -9,28 +9,33 @@ public class ConvolutionProviderImpl implements ConvolutionProvider {
     public Color[][] apply(Color[][] image, double[][] kernel) {
 //      Here we assume that kernel is rectangular and has same height and width
         int kernelRadius = kernel.length / 2;
+        int columnLength = image.length;
+        var result = new Color[columnLength][];
 
-//        var result = image.clone(); //Yeah, doesn't work since it's not true 2d and we'd end up with same values
-        var result = new Color[image.length][];
+        for (var i = 0; i < columnLength; i++) {
+            int rowLength = image[i].length;
+            result[i] = new Color[rowLength];
+            for (var j = 0; j < rowLength; j++) {
 
-        for (var i = 0; i < image.length; i++) {
-            result[i] = new Color[image[i].length];
-            for (var j = 0; j < image[i].length; j++) {
-
-                int r = 0, g = 0, b = 0;
+                int r = 0;
+                int g = 0;
+                int b = 0;
                 for (var y = 0; y < kernel.length; y++) {
                     for (var x = 0; x < kernel[y].length; x++) {
 
                         int imageX = j + kernelRadius - x;
                         int imageY = i + kernelRadius - y;
 
-                        if (imageX < 0 || imageX >= image[i].length ||
-                                imageY < 0 || imageY >= image.length)
+                        if (imageX < 0 || imageX >= rowLength ||
+                                imageY < 0 || imageY >= columnLength) {
                             continue;
+                        }
 
-                        r += image[imageY][imageX].getRed() * kernel[y][x];
-                        g += image[imageY][imageX].getGreen() * kernel[y][x];
-                        b += image[imageY][imageX].getBlue() * kernel[y][x];
+                        Color imageColor = image[imageY][imageX];
+                        double kernelPixel = kernel[y][x];
+                        r += imageColor.getRed() * kernelPixel;
+                        g += imageColor.getGreen() * kernelPixel;
+                        b += imageColor.getBlue() * kernelPixel;
                     }
                 }
                 result[i][j] = new Color(r, g, b);
