2ebcaf6aa5cb0dc0a00cf53994b52ced7c86d1cd
ITLeopard <forest_spbu@mail.ru>
2020-02-24 18:40:14 +0300

    Реализован метод convertToColor интерфейса ImageConverter

--- src/main/java/impl/image/ImageConverterFactory.java
+++ src/main/java/impl/image/ImageConverterFactory.java
@@ -4,6 +4,6 @@ import api.image.ImageConverter;
 
 public class ImageConverterFactory {
     public static ImageConverter getInstance() {
-        return null;
+        return new ImageConveterImplementation();   // возвращаем объект, в котором реализован интерфейс ImageConverter
     }
 }
