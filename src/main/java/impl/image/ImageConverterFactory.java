0c8f48379a4e14ebc97015506f7514778e22935f
lennuz17 <lennuz17@gmail.com>
2020-02-24 20:53:23 +0300

    Add Lab 1

--- src/main/java/impl/image/ImageConverterFactory.java
+++ src/main/java/impl/image/ImageConverterFactory.java
@@ -2,8 +2,34 @@ package impl.image;
 
 import api.image.ImageConverter;
 
+import java.awt.*;
+
 public class ImageConverterFactory {
     public static ImageConverter getInstance() {
-        return null;
+
+        ImageConverter imageConverter = new ImageConverter() {
+            @Override
+            public Color[][] convertToColor(int[][] image) {
+                Color[][] colors  = new Color[image.length][image[0].length];
+                for (int row = 0; row < image.length; row++) {
+                    for (int column = 0; column<image[0].length; column++){
+                        colors[row][column] = new Color(image[row][column]);
+                    }
+                }
+                return colors;
+            }
+
+            @Override
+            public int[][] convertToRgb(Color[][] image) {
+                int[][] pixelColor = new int[image.length][image[0].length];
+                for (int row =0; row<image.length; row++){
+                    for (int column = 0; column<image[row].length; column++){
+                        pixelColor[row][column] = image[row][column].getRGB();
+                    }
+                }
+                return pixelColor;
+            }
+        };
+        return imageConverter;
     }
 }
