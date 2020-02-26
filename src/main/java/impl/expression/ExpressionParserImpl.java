4d583508e3c6f276c02e78bb1f0f54bf2556a603
Kirill Smirnov <Smirnov.K@numdes.com>
2020-02-26 19:25:37 +0300

    Implement expression parsing and calculation
    On branch kirill-smirnov-task-2

--- src/main/java/impl/expression/ExpressionParserImpl.java
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -3,6 +3,8 @@ package impl.expression;
 import api.expression.ExpressionParser;
 import api.expression.ParseException;
 
+import java.util.Optional;
+
 public class ExpressionParserImpl implements ExpressionParser {
 
     @Override
@@ -46,13 +48,39 @@ public class ExpressionParserImpl implements ExpressionParser {
             number.append(digit);
         }
 
-        public void processSign(Sign newSign) {
+        public void processSign(Sign newSign) throws ParseException {
+            executeAddition();
             sign = newSign;
             number = null;
         }
 
-        public int getResult() {
+        public int getResult() throws ParseException {
+            executeAddition();
             return result;
+        }
+
+        private void executeAddition() throws ParseException {
+            Optional<Integer> currentNumber = getCurrentNumber();
+            if (currentNumber.isPresent()) {
+                if (sign == Sign.MINUS) {
+                    result -= currentNumber.get();
+                } else {
+                    result += currentNumber.get();
+                }
+            } else if (sign != null) {
+                throw new ParseException("Several signs in a row are not permitted");
+            }
+        }
+
+        private Optional<Integer> getCurrentNumber() throws ParseException {
+            if (number == null || number.length() == 0) {
+                return Optional.empty();
+            }
+            try {
+                return Optional.of(Integer.parseInt(number.toString()));
+            } catch (Exception e) {
+                throw new ParseException("Unexpected number: " + e.getMessage());
+            }
         }
     }
 }
