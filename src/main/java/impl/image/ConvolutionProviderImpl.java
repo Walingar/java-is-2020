b4d2adaac481c60ae3b63a9115a9f21b2521a436
Sergey Stepanov <stepanov.s@numdes.com>
2020-02-27 19:26:33 +0300

    Work in progress Add ConvolutionProviderImpl

---
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -0,0 +1,12 @@
+package impl.image;
+
+import api.image.ConvolutionProvider;
+
+import java.awt.*;
+
+public class ConvolutionProviderImpl implements ConvolutionProvider {
+    @Override
+    public Color[][] apply(Color[][] image, double[][] kernel) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+}
