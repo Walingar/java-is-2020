c305b1030a21b3c7551c9eba89106681e74aaeef
Andy1897 <34788382+Andy1897@users.noreply.github.com>
2020-02-27 23:11:57 +0300

    Task1

--- src/main/java/impl/image/ImageConverterFactory.java
+++ src/main/java/impl/image/ImageConverterFactory.java
@@ -1,9 +1,37 @@
 package impl.image;
 
-import api.image.ImageConverter;
+import java.awt.*;
 
 public class ImageConverterFactory {
-    public static ImageConverter getInstance() {
-        return null;
+    public static ImageConverterImpl getInstance() {
+        return new ImageConverterImpl();
+    }
+
+    public static class ImageConverterImpl implements api.image.ImageConverter {
+        @Override
+        public Color[][] convertToColor(int[][] image) {
+            int imgHeight = image.length;
+            int imgWidth = image[0].length;
+            Color[][] resultColor = new Color[imgHeight][imgWidth];
+            for (int i = 0; i < imgHeight; i++) {
+                for (int j = 0; j < imgWidth; j++) {
+                    resultColor[i][j] = new Color(image[i][j]);
+                }
+            }
+            return resultColor;
+        }
+
+        @Override
+        public int[][] convertToRgb(Color[][] image) {
+            int imgHeight = image.length;
+            int imgWidth = image[0].length;
+            int[][] resultRGB = new int[imgHeight][imgWidth];
+            for (int i = 0; i < imgHeight; i++) {
+                for (int j = 0; j < imgWidth; j++) {
+                    resultRGB[i][j] = image[i][j].getRGB();
+                }
+            }
+            return resultRGB;
+        }
     }
 }
