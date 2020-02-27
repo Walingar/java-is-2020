dc3afac8ec5c20cf9b908e117a8994b9a6dbc9f5
Alexander Ivchenko <A.S.Ivchenko@yandex.ru>
2020-02-27 11:22:30 +0300

    Simplify converter by using existing in Color methods

--- src/main/java/impl/image/ImageConverterImpl.java
+++ src/main/java/impl/image/ImageConverterImpl.java
@@ -1,17 +1,10 @@
 package impl.image;
 
 import api.image.ImageConverter;
-import org.jetbrains.annotations.TestOnly;
 
 import java.awt.*;
 
 public final class ImageConverterImpl implements ImageConverter {
-    private static final int BYTE_MASK = 0xff;
-    private static final int BLUE_CHANNEL_OFFSET = 0;
-    private static final int GREEN_CHANNEL_OFFSET = 8;
-    private static final int RED_CHANNEL_OFFSET = 16;
-    private static final int ALPHA_CHANNEL_OFFSET = 24;
-
     @Override
     public Color[][] convertToColor(int[][] image) {
         if (image.length == 0) {
@@ -22,19 +15,10 @@ public final class ImageConverterImpl implements ImageConverter {
         Color[][] colors = new Color[height][width];
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                colors[i][j] = convertToColor(image[i][j]);
+                colors[i][j] = new Color(image[i][j], true);
             }
         }
         return colors;
-    }
-
-    @TestOnly
-    Color convertToColor(int rgb) {
-        int red = (rgb >>> RED_CHANNEL_OFFSET) & BYTE_MASK;
-        int green = (rgb >>> GREEN_CHANNEL_OFFSET) & BYTE_MASK;
-        int blue = (rgb >>> BLUE_CHANNEL_OFFSET) & BYTE_MASK;
-        int alpha = (rgb >>> ALPHA_CHANNEL_OFFSET) & BYTE_MASK;
-        return new Color(red, green, blue, alpha);
     }
 
     @Override
@@ -47,21 +31,9 @@ public final class ImageConverterImpl implements ImageConverter {
         int[][] rgbs = new int[height][width];
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                rgbs[i][j] = convertToRgb(image[i][j]);
+                rgbs[i][j] = image[i][j].getRGB();
             }
         }
         return rgbs;
-    }
-
-    @TestOnly
-    int convertToRgb(Color color) {
-        int red = color.getRed();
-        int green = color.getGreen();
-        int blue = color.getBlue();
-        int alpha = color.getAlpha();
-        return (red << RED_CHANNEL_OFFSET) +
-                (green << GREEN_CHANNEL_OFFSET) +
-                (blue << BLUE_CHANNEL_OFFSET) +
-                (alpha << ALPHA_CHANNEL_OFFSET);
     }
 }
