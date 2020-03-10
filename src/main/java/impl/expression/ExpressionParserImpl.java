36af2491e71c45538b0d7f6644ca8f397f32f4d9
NeHo4y <whideofficial@gmail.com>
2020-03-10 22:24:37 +0300

    [task 2] Implement ExpressionParser

---
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -0,0 +1,66 @@
+package impl.expression;
+
+import api.expression.ExpressionParser;
+import api.expression.ParseException;
+
+public class ExpressionParserImpl implements ExpressionParser {
+    StringBuilder currentNumber;
+    char sign;
+
+    public ExpressionParserImpl() {
+        currentNumber = new StringBuilder();
+        sign = '+';
+    }
+
+    public int parse(String expression) throws ParseException {
+
+        if (expression == null) {
+            throw new IllegalArgumentException("Expression is null");
+        }
+        if (expression.isBlank()) {
+            throw new IllegalArgumentException("Expression is empty");
+        }
+        int result = 0;
+        for (int pos = 0; pos < expression.length(); pos++) {
+
+            char processed_char = expression.charAt(pos);
+            if (Character.isWhitespace(processed_char)) {
+                continue;
+            }
+            if (Character.isDigit(processed_char)) {
+                currentNumber.append(processed_char);
+            } else {
+                if (processed_char == '+' || processed_char == '-') {
+                    if (currentNumber.length() != 0) {
+                        result = calculate(result, getValue(currentNumber));
+                    }
+                    currentNumber.append(processed_char);
+                } else {
+                    throw new ParseException("Unknown character encountered during parsing in position: " + pos);
+                }
+            }
+        }
+        if (currentNumber.length() != 0){
+            result = calculate(result, getValue(currentNumber));
+        }
+        return result;
+    }
+
+    private int getValue(StringBuilder number) throws ParseException {
+        try {
+            var stringNumber = number.toString();
+            return Integer.parseInt(stringNumber);
+        } catch (NumberFormatException e) {
+            throw new ParseException("Non-valid integer format");
+        }
+    }
+
+    private int calculate(int a, int b) throws ArithmeticException {
+        currentNumber.setLength(0);
+        try {
+            return Math.addExact(a, b);
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("Unable to compute math operation");
+        }
+    }
+}
