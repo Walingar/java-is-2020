1103971ec2412ad0f8cc9ec0b4b7fb121181401e
maxxkut <maksim.kutimskiy@gmail.com>
2020-03-11 11:36:25 +0300

    Task 2. fix ExpressionParser implementation

--- src/main/java/impl/expression/ExpressionParserImpl.java
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -10,46 +10,50 @@ public class ExpressionParserImpl implements ExpressionParser {
         if (expression == null) {
             throw new IllegalArgumentException("Expression is null");
         }
-        expression = expression.replaceAll(" ", "");
+        expression = expression.replaceAll("\\s", "");
+        if (expression.isBlank()) {
+            throw new IllegalArgumentException("Expression is empty");
+        }
         int result = 0;
-        char sign = '.';
         StringBuilder currentNumber = new StringBuilder();
         //проход по строке
         for (int index = 0; index < expression.length(); index++) {
             char currentChar = expression.charAt(index);
             // Дописываем цифру в строку
-            if ('0' <= currentChar && currentChar <= '9') {
+            if (Character.isDigit(currentChar)) {
                 currentNumber.append(currentChar);
                 continue;
             }
             // если знак
             if (currentChar == '+' || currentChar == '-') {
-                if (sign == '.') {
-                    result += getNumber(currentNumber, '+');
+                if(currentNumber.length() != 0){
+                    result = addValue(result, getNumber(currentNumber));
+                    currentNumber = new StringBuilder();
                 }
-                if (sign == '+' || sign == '-') {
-                    result += getNumber(currentNumber, sign);
-                }
-                sign = currentChar;
-                currentNumber = new StringBuilder();
-                continue;
-            }
-            // если конец строки
-            if (index == expression.length() - 1) {
-                result += getNumber(currentNumber, sign);
-                currentNumber = new StringBuilder();
+                currentNumber.append(currentChar);
                 continue;
             }
             throw new ParseException(String.format("Illegal character '%c' ", currentChar));
         }
+        if (currentNumber.length() != 0) {
+            result = addValue(result, getNumber(currentNumber));
+        }
         return result;
     }
 
-    private int getNumber(StringBuilder currentNumber, char sign) throws ParseException {
-        if (currentNumber == null || currentNumber.length() == 0) {
-            throw new ParseException("More than one sign in a row are not permitted");
+    private int getNumber(StringBuilder currentNumber) throws ParseException {
+        try {
+            return Integer.parseInt(currentNumber.toString());
+        } catch (NumberFormatException e){
+            throw new ParseException("Not valid integer format");
         }
-        int current = Integer.parseInt(currentNumber.toString());
-        return '-' == sign ? -current : current;
+    }
+
+    private int addValue(int a, int b) throws ArithmeticException {
+        try {
+            return Math.addExact(a, b);
+        } catch (ArithmeticException e){
+            throw new ArithmeticException("Math operation error");
+        }
     }
 }
