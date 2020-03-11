352a2eb8cea2471d36cd91ec777256b81bd5450e
Alex Zhurba <alex7ey1@gmail.com>
2020-03-11 17:33:44 +0300

    Introduce implementation of ExpressionParser

---
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -0,0 +1,58 @@
+package impl.expression;
+
+import api.expression.ExpressionParser;
+import api.expression.ParseException;
+
+public class ExpressionParserImpl implements ExpressionParser {
+    @Override
+    public int parse(String expression) throws ParseException {
+
+        if (expression == null) {
+            throw new IllegalArgumentException("Expression is null");
+        }
+
+        int result = 0;
+        int tempOperand = 0;
+        int operator = 1;
+
+        char currentChar;
+
+        for (int i = 0; i < expression.length(); i++) {
+
+            currentChar = expression.charAt(i);
+
+            if (Character.isWhitespace(currentChar)) {
+                continue;
+            }
+
+            if (currentChar == '-' || currentChar == '+') {
+                result = updateResult(result, operator, tempOperand);
+                tempOperand = 0;
+                if (currentChar == '-') {
+                    operator = -1;
+                } else {
+                    operator = 1;
+                }
+            }  else if (Character.isDigit(currentChar)) {
+                try {
+                    tempOperand = Math.addExact(Math.multiplyExact(tempOperand, 10), Character.getNumericValue(currentChar));
+                } catch (ArithmeticException e) {
+                    throw new ParseException("Too long number");
+                }
+            } else {
+                throw new ParseException("Invalid symbol");
+            }
+        }
+        result = updateResult(result, operator, tempOperand);
+
+        return result;
+    }
+
+    private int updateResult(int operandA, int operator, int operandB) throws ArithmeticException {
+        try {
+            return Math.addExact(operandA, Math.multiplyExact(operator, operandB));
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("Overflow");
+        }
+    }
+}
