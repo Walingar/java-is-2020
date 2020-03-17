5b5d1af7f7295e374882e1da977ec5ce1e944a1f
chausov <dxahtepb@gmail.com>
2020-03-17 17:03:58 +0300

    [task-3] Introduce some improvements to YearTemperatureStats
     - Rename some variables to more suitable ones (auto-refactor)

--- src/main/java/impl/weather/YearTemperatureStatsImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsImpl.java
@@ -8,25 +8,25 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class YearTemperatureStatsImpl implements YearTemperatureStats {
-    private final Map<Month, MonthTemperatureInfo> map = new EnumMap<>(Month.class);
+    private final Map<Month, MonthTemperatureInfo> monthInfoMap = new EnumMap<>(Month.class);
 
     @Override
     public void updateStats(DayTemperatureInfo info) {
         Objects.requireNonNull(info, "Info should not be null");
         var month = info.getMonth();
-        map.computeIfAbsent(month, key -> new MonthTemperatureInfo()).updateStats(info);
+        monthInfoMap.computeIfAbsent(month, key -> new MonthTemperatureInfo()).updateStats(info);
     }
 
     @Override
     public Double getAverageTemperature(Month month) {
         Objects.requireNonNull(month, "Month should not be null");
-        var monthInfo = map.get(month);
+        var monthInfo = monthInfoMap.get(month);
         return (monthInfo == null) ? null : monthInfo.getAverageTemperature();
     }
 
     @Override
     public Map<Month, Integer> getMaxTemperature() {
-        return map.entrySet().stream()
+        return monthInfoMap.entrySet().stream()
                 .collect(Collectors.toUnmodifiableMap(
                         Map.Entry::getKey,
                         entry -> entry.getValue().getMaxTemperature()
@@ -36,14 +36,14 @@ public class YearTemperatureStatsImpl implements YearTemperatureStats {
     @Override
     public List<DayTemperatureInfo> getSortedTemperature(Month month) {
         Objects.requireNonNull(month, "Month should not be null");
-        var monthInfo = map.get(month);
+        var monthInfo = monthInfoMap.get(month);
         return (monthInfo == null) ? Collections.emptyList() : monthInfo.getSortedTemperature();
     }
 
     @Override
     public DayTemperatureInfo getTemperature(int day, Month month) {
         Objects.requireNonNull(month, "Month should not be null");
-        var monthInfo = map.get(month);
+        var monthInfo = monthInfoMap.get(month);
         return (monthInfo == null) ? null : monthInfo.getTemperature(day);
     }
 }
