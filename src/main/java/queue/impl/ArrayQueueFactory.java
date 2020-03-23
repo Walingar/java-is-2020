cf6c72477b72408add936c59a1ba67cd6d4b5352
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2020-03-23 19:59:04 +0300

    [task-4] Introduce factories and tests for Queue interface

--- src/main/java/queue/impl/ArrayQueueFactory.java
+++ src/main/java/queue/impl/ArrayQueueFactory.java
@@ -4,6 +4,10 @@ import java.util.Queue;
 
 public class ArrayQueueFactory {
     public static Queue<Integer> getInstance() {
+<<<<<<< HEAD
         return new ArrayQueueImpl();
+=======
+        return null;
+>>>>>>> eca6530... [task-4] Introduce factories and tests for Queue interface
     }
 }
--- src/main/java/queue/impl/LinkedQueueFactory.java
+++ src/main/java/queue/impl/LinkedQueueFactory.java
@@ -4,6 +4,10 @@ import java.util.Queue;
 
 public class LinkedQueueFactory {
     public static Queue<Integer> getInstance() {
+<<<<<<< HEAD
         return new LinkedQueueImpl();
+=======
+        return null;
+>>>>>>> eca6530... [task-4] Introduce factories and tests for Queue interface
     }
 }
