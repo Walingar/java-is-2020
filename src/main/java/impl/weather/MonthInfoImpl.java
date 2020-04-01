068e3dd3c5643c215b745a450c4a0c8734e22d7a
anastasiiatitova <staceylemann@gmail.com>
2020-04-01 15:47:49 +0300

    back to map

--- src/main/java/impl/weather/MonthInfoImpl.java
+++ src/main/java/impl/weather/MonthInfoImpl.java
@@ -4,40 +4,37 @@ import api.weather.DayTemperatureInfo;
 import api.weather.MonthInfo;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MonthInfoImpl implements MonthInfo {
     private int maximum = Integer.MIN_VALUE;
     private double average = 0;
-    private final List<DayTemperatureInfo> days = new ArrayList<>();
+    private final Map<Integer, DayTemperatureInfo> days = new LinkedHashMap<>();
 
     public void addDay(DayTemperatureInfo info) {
         var day = info.getDay();
         var temperature = info.getTemperature();
-
-        var oldDay = getDay(day);
-        if (oldDay == null) {
-            updateAverageNewDay(temperature);
-            days.add(info);
-        } else {
-            updateAverageExistingDay(oldDay.getTemperature(), temperature);
-            days.set(days.indexOf(oldDay), info);
-        }
         updateMaximum(temperature);
+        updateAverage(day, temperature);
+        days.put(day, info);
     }
 
     private void updateMaximum(Integer temperature) {
         maximum = Math.max(maximum, temperature);
     }
 
-    private void updateAverageExistingDay(Integer oldTemperature, Integer temperature) {
+    private void updateAverage(Integer day, Integer temperature) {
         var size = days.size();
-        average = (average * size - oldTemperature + temperature) / size;
-    }
-
-    private void updateAverageNewDay(Integer temperature) {
-        var size = days.size();
-        average = (average * size + temperature) / (size + 1);
+        if (days.containsKey(day)) {
+            // updated day info
+            var oldTemperature = days.get(day).getTemperature();
+            average = (average * size - oldTemperature + temperature) / size;
+        } else {
+            // new day
+            average = (average * size + temperature) / (size + 1);
+        }
     }
 
     public double getAverage() {
@@ -49,10 +46,10 @@ public class MonthInfoImpl implements MonthInfo {
     }
 
     public DayTemperatureInfo getDay(Integer day) {
-        return days.stream().filter(dayInfo -> dayInfo.getDay() == day).findFirst().orElse(null);
+        return days.getOrDefault(day, null);
     }
 
     public List<DayTemperatureInfo> getDays() {
-        return days;
+        return new ArrayList<>(days.values());
     }
 }
