e836b6109acd34906af2e8ddd7a461c9376b5a9d
chausov <dxahtepb@gmail.com>
2020-04-01 23:55:52 +0300

    [task-5] Implement FileEncodingReader and FileEncodingWriter

---
+++ src/main/java/impl/file/FileEncodingReaderImpl.java
@@ -0,0 +1,22 @@
+package impl.file;
+
+import api.file.FileEncodingReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
+public class FileEncodingReaderImpl implements FileEncodingReader {
+    @Override
+    public Reader read(File file, Charset fileEncoding) {
+        try {
+            return new BufferedReader(new FileReader(file, fileEncoding));
+        } catch (IOException e) {
+            System.err.println("Error occurred while file opening: " + e);
+        }
+        return Reader.nullReader();
+    }
+}
