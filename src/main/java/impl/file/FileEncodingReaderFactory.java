79629d722e6b3e9a568c93d80df556e59b00e18b
lennuz17 <lennuz17@gmail.com>
2020-04-06 02:35:38 +0300

    HW 5

--- src/main/java/impl/file/FileEncodingReaderFactory.java
+++ src/main/java/impl/file/FileEncodingReaderFactory.java
@@ -2,8 +2,27 @@ package impl.file;
 
 import api.file.FileEncodingReader;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
 public class FileEncodingReaderFactory {
     public static FileEncodingReader getInstance() {
-        return null;
+        var fileReader = new FileEncodingReader() {
+
+            @Override
+            public Reader read(File file, Charset fileEncoding) {
+                Reader reader = null;
+                try {
+                    reader = new FileReader(file, fileEncoding);
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+                return reader;
+            }
+        };
+        return fileReader;
     }
 }
--- src/main/java/impl/file/FileEncodingWriterFactory.java
+++ src/main/java/impl/file/FileEncodingWriterFactory.java
@@ -2,8 +2,53 @@ package impl.file;
 
 import api.file.FileEncodingWriter;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 public class FileEncodingWriterFactory {
     public static FileEncodingWriter getInstance() {
-        return null;
+        var fileWriter = new FileEncodingWriter() {
+            //`FileEncodingWriter` должен уметь создавать файл и
+// записывать в него данные из `InputStream` с кодировкой `dataEncoding` в кодировке `fileEncoding` (`UTF-8`, если `fileEncoding` не передана).
+            @Override
+            public void write(File file, InputStream data, Charset dataEncoding) {
+                int c = 0;
+                createFile(file);
+                try (Writer writer = new FileWriter(file, StandardCharsets.UTF_8);
+                     InputStreamReader inputStreamReader = new InputStreamReader(data, dataEncoding)){
+                    while ((c = inputStreamReader.read()) >= 0) {
+                        writer.write(c);
+                    }
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+
+            @Override
+            public void write(File file, InputStream data, Charset dataEncoding, Charset fileEncoding) {
+                createFile(file);
+                var c = 0;
+                try (Writer writer = new FileWriter(file, fileEncoding);
+                     InputStreamReader inputStreamReader = new InputStreamReader(data, dataEncoding)){
+                    while ((c = inputStreamReader.read()) >= 0) {
+                        writer.write(c);
+                    }
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+
+            private void createFile(File file) {
+                try {
+                    var dirs = file.getParentFile();
+                    dirs.mkdirs();
+                    dirs.createNewFile();
+                } catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        };
+        return fileWriter;
     }
 }
