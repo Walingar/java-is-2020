82cc2bb1feef8fbbbfff08024e481f8de1a7412c
be39na <61196684+be39na@users.noreply.github.com>
2020-04-08 22:56:15 +0300

    added implementtation of queue Integer on linked list and array

---
+++ src/main/java/queue/impl/LinkedQueueImpl.java
@@ -0,0 +1,96 @@
+package queue.impl;
+
+import java.util.*;
+
+public class LinkedQueueImpl extends AbstractQueue<Integer> {
+    private static class Item {
+        private Integer value;
+        private Item nextItem;
+
+        Item(Item next, int value) {
+            this.nextItem = next;
+            this.value = value;
+        }
+
+        Integer getValue() {
+            return value;
+        }
+
+        void setValue(int newValue) {
+            this.value = newValue;
+        }
+
+        Item getNext() {
+            return nextItem;
+        }
+
+        void setNext(Item next) {
+            this.nextItem = next;
+        }
+    }
+
+    private int size = 0;
+    private Item head = null;
+    private Item tail = null;
+
+    private static class LinkedQueueIterator implements Iterator {
+        Item current;
+        LinkedQueueIterator(Item head) {
+            current = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current.getNext() != null;
+        }
+
+        @Override
+        public Item next() {
+            var next = current.getNext();
+            return next;
+        }
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new LinkedQueueIterator(head);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean offer(Integer Item) {
+        Item newItem = new Item(null, Item);
+        if (head == null) {
+            head = newItem;
+            tail = head;
+        } else {
+            tail.setNext(newItem);
+            tail = newItem;
+        }
+        size++;
+        return true;
+    }
+
+    @Override
+    public Integer poll() {
+        if (size == 0) {
+            return null;
+        }
+        Integer value = head.getValue();
+        head = head.getNext();
+        size--;
+        return value;
+    }
+
+    @Override
+    public Integer peek() {
+        if (size == 0) {
+            return null;
+        }
+        return head.getValue();
+    }
+}
