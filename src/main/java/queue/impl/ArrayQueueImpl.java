31822bbc9dff5b249be671b2b4b723d384b8f307
be39na <61196684+be39na@users.noreply.github.com>
2020-04-08 22:56:15 +0300

    added implementtation of queue Integer on linked list and array

---
+++ src/main/java/queue/impl/ArrayQueueImpl.java
@@ -0,0 +1,90 @@
+package queue.impl;
+
+import java.util.AbstractQueue;
+import java.util.*;
+
+public class ArrayQueueImpl extends AbstractQueue<Integer> {
+    private Integer[] queue;
+    private int capacity;
+    private int size;
+    private int head;
+    private int tail;
+    private int resizeCoefficient;
+
+    ArrayQueueImpl() {
+        capacity = 1;
+        resizeCoefficient = 2;
+        queue = new Integer[capacity];
+        size = 0;
+        head = 0;
+        tail = 0;
+    }
+
+    private class ArrayQueueIterator implements Iterator {
+        private int currentIndex;
+
+        ArrayQueueIterator() {
+            currentIndex = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < tail;
+        }
+
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            currentIndex++;
+            return queue[currentIndex];
+        }
+    }
+
+    private void resizeQueue() {
+        capacity *= resizeCoefficient;
+        Integer[] buffer = new Integer[capacity];
+        System.arraycopy(queue, 0, buffer, 0, queue.length);
+        queue = buffer;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new ArrayQueueIterator();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean offer(Integer item) {
+        if (tail == capacity) {
+            resizeQueue();
+        }
+        queue[tail] = (Integer) item;
+        size++;
+        tail++;
+        return true;
+    }
+
+    @Override
+    public Integer poll() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        return queue[head++];
+
+    }
+
+    @Override
+    public Integer peek() {
+        if (size() == 0) {
+            return null;
+        }
+        return queue[head];
+    }
+}
