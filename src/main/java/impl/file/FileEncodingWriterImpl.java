85b5d7674ca97dd75ed822bf39d1e676e7785d76
Mike Saravayskiy <maiksaray@gmail.com>
2020-04-11 16:19:17 +0300

    Implemented FileEncodingReader and FileEncodingWriter

---
+++ src/main/java/impl/file/FileEncodingWriterImpl.java
@@ -0,0 +1,43 @@
+package impl.file;
+
+import api.file.FileEncodingWriter;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class FileEncodingWriterImpl implements FileEncodingWriter {
+
+    private final Charset defaultCharset;
+
+    public FileEncodingWriterImpl() {
+        defaultCharset = StandardCharsets.UTF_8;
+    }
+
+    @Override
+    public void write(File file, InputStream data, Charset dataEncoding) {
+        write(file, data, dataEncoding, defaultCharset);
+    }
+
+    @Override
+    public void write(File file, InputStream data, Charset dataEncoding, Charset fileEncoding) {
+        var parent = file.getParentFile();
+        if (parent != null && !parent.exists()) {
+            if(!parent.mkdirs()){
+                // It makes sense to throw here... Hmm...
+                System.err.println("Could not create directory for the file");
+            }
+        }
+
+        try (var reader = new InputStreamReader(data, dataEncoding);
+             var writer = new FileWriter(file, fileEncoding)) {
+            var s = reader.read();
+            while (s != -1) {
+                writer.write(s);
+                s = reader.read();
+            }
+        } catch (IOException e) {
+            System.err.println(String.format("Something went wrong during I/O: %s", e.getMessage()));
+        }
+    }
+}
