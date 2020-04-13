719b4b8c8bbfbf81b83f17a607e38bac5ba9a005
Nikolay Rykunov <Nikolay.Rykunov@jetbrains.com>
2020-04-13 23:00:34 +0300

    [task-6] Introduce Pair and NumberPair interfaces

---
+++ src/main/java/impl/pair/NumberPairFactory.java
@@ -0,0 +1,9 @@
+package impl.pair;
+
+import api.pair.Pair;
+
+public class NumberPairFactory {
+    public static <K extends Number, T extends Number> Pair<K, T> of(K first, T second) {
+        return null;
+    }
+}
