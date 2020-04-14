44034aef86f4628a0412143b15df2bcf6326893f
Mike Saravayskiy <maiksaray@gmail.com>
2020-04-14 22:50:56 +0300

    Implemented Pair with PairImpl Didn't get NumberPair issue...

---
+++ src/main/java/impl/pair/PairImpl.java
@@ -0,0 +1,47 @@
+package impl.pair;
+
+import api.pair.Pair;
+
+import java.util.Objects;
+
+public class PairImpl<F, S> implements Pair<F, S> {
+    private final F first;
+    private final S second;
+
+    public PairImpl(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public F getFirst() {
+        return first;
+    }
+
+    @Override
+    public S getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "PairImpl{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PairImpl)) return false;
+        PairImpl<?, ?> pair = (PairImpl<?, ?>) o;
+        return Objects.equals(getFirst(), pair.getFirst()) &&
+                Objects.equals(getSecond(), pair.getSecond());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirst(), getSecond());
+    }
+}
