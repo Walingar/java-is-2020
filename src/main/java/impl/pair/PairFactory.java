d65dd62440e9a0655120250f8d2502ac34b491df
Ammar Ali <35392687+ammarali32@users.noreply.github.com>
2020-04-14 14:36:10 +0300

    Task6

--- src/main/java/impl/pair/PairFactory.java
+++ src/main/java/impl/pair/PairFactory.java
@@ -1,7 +1,9 @@
 package impl.pair;
 
+import api.pair.Pair;
+
 public class PairFactory {
-    public static <K, T> PairImpl of(K first, T second) {
+    public static <K, T> Pair<K, T> of(K first, T second) {
         return new PairImpl(first, second);
     }
 }
