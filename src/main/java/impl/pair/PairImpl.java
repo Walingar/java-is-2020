1246f6e553f780adfb5853b86cbb56d52f317e75
chausov <dxahtepb@gmail.com>
2020-04-14 07:02:56 +0300

    [task-6] Implement Pair<T, K> interface

---
+++ src/main/java/impl/pair/PairImpl.java
@@ -0,0 +1,48 @@
+package impl.pair;
+
+import api.pair.Pair;
+
+import java.util.Objects;
+
+public class PairImpl<T, K> implements Pair<T, K> {
+    private final T first;
+    private final K second;
+
+    PairImpl(T first, K second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public T getFirst() {
+        return first;
+    }
+
+    @Override
+    public K getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PairImpl<?, ?> pair = (PairImpl<?, ?>) o;
+        return Objects.equals(getFirst(), pair.getFirst()) &&
+                Objects.equals(getSecond(), pair.getSecond());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirst(), getSecond());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Pair{first=%s, second=%s}", first, second);
+    }
+}
