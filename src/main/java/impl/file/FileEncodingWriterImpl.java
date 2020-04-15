3f433ce455272d0d160fce037083d46cf66db4ca
Aleksandr Aleksandrov <18685492+AD-Aleksandrov@users.noreply.github.com>
2020-04-15 23:30:42 +0300

    [Task-5] Aleksandr Aleksandrov impl

---
+++ src/main/java/impl/file/FileEncodingWriterImpl.java
@@ -0,0 +1,39 @@
+package impl.file;
+
+import api.file.FileEncodingWriter;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class FileEncodingWriterImpl implements FileEncodingWriter {
+    private static final int IO_BUFF_SIZE = 4096;
+    private static final Charset DEFAULT_FILE_ENCODING = StandardCharsets.UTF_8;
+
+    public void write(File file, InputStream data, Charset dataEncoding) {
+        write(file, data, dataEncoding, DEFAULT_FILE_ENCODING);
+    }
+
+    public void write(File file, InputStream data, Charset dataEncoding, Charset fileEncoding) {
+        File file_dir = file.getParentFile();
+
+        if (file_dir != null && !file_dir.exists() && !file_dir.mkdirs()){
+            throw new RuntimeException("Failed to create parent directory");
+        }
+
+        try {
+            FileWriter file_stream = new FileWriter(file, fileEncoding);
+            InputStreamReader reader = new InputStreamReader(data, dataEncoding);
+            char []buff = new char[IO_BUFF_SIZE];
+            int read_bytes;
+
+            while ((read_bytes = reader.read(buff, 0, buff.length)) != -1) {
+                file_stream.write(buff, 0, read_bytes);
+            }
+
+            file_stream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
