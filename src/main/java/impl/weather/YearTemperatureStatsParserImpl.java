a4b26b45c008e74eb84533f22be93abc4102d932
NeHo4y <whideofficial@gmail.com>
2020-04-15 17:50:26 +0300

    [task 3] Fix review issues

--- src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
@@ -15,22 +15,22 @@ public class YearTemperatureStatsParserImpl implements YearTemperatureStatsParse
         YearTemperatureStats temperatureStats = YearTemperatureStatsFactory.getInstance();
 
         for (var rawDayData : rawData) {
-            var splittedTokens = rawDayData.split(" ");
-            if (splittedTokens.length != 2) {
+            var splitTokens = rawDayData.split(" ");
+            if (splitTokens.length != 2) {
                 throw new IllegalArgumentException("wrong data format");
             }
 
-            var splittedDate = splittedTokens[0].split("\\.");
+            var splittedDate = splitTokens[0].split("\\.");
             if (splittedDate.length != 2) {
                 throw new IllegalArgumentException("wrong date format");
             }
             try {
                 var day = Integer.parseInt(splittedDate[0]);
                 var month = Integer.parseInt(splittedDate[1]);
-                var temperature = Integer.parseInt(splittedTokens[1]);
+                var temperature = Integer.parseInt(splitTokens[1]);
                 temperatureStats.updateStats(new DayTemperatureInfoImpl(day, month, temperature));
             } catch (NumberFormatException e) {
-                throw new ParseError("Illegal number format format");
+                throw new ParseException("Illegal number format format");
             }
         }
         return temperatureStats;
