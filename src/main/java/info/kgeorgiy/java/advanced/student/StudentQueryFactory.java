3cc96dc2c380b0370ff0e4d9c67b25d5bd1a9a8a
Alexander Sukhikh <xbox24_772@mail.ru>
2020-04-16 02:18:48 +0300

    StudentQueryImpl created. All methods implemented.

--- src/main/java/info/kgeorgiy/java/advanced/student/StudentQueryFactory.java
+++ src/main/java/info/kgeorgiy/java/advanced/student/StudentQueryFactory.java
@@ -2,6 +2,6 @@ package info.kgeorgiy.java.advanced.student;
 
 public class StudentQueryFactory {
     public static StudentQuery getInstance() {
-        return null;
+        return new StudentQueryImpl();
     }
 }
