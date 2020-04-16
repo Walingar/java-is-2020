8ee94f1282fd249b8f9921607eeba0e9d170f818
Alexander Sukhikh <xbox24_772@mail.ru>
2020-04-16 03:36:05 +0300

    Got rid of repetitive code. All tests passed.

--- src/main/java/info/kgeorgiy/java/advanced/student/StudentQueryImpl.java
+++ src/main/java/info/kgeorgiy/java/advanced/student/StudentQueryImpl.java
@@ -4,92 +4,67 @@ import java.util.*;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class StudentQueryImpl implements StudentQuery {
 
 
     @Override
     public List<String> getFirstNames(List<Student> students) {
-        return students.stream()
-                .map(Student::getFirstName)
-                .collect(Collectors.toList());
+        return (List<String>) getMappedCollection(students, Student::getFirstName);
     }
 
     @Override
     public List<String> getLastNames(List<Student> students) {
-        return students.stream()
-                .map(Student::getLastName)
-                .collect(Collectors.toList());
+        return (List<String>) getMappedCollection(students, Student::getLastName);
     }
 
     @Override
     public List<String> getGroups(List<Student> students) {
-        return students.stream()
-                .map(Student::getGroup)
-                .collect(Collectors.toList());
+        return (List<String>) getMappedCollection(students, Student::getGroup);
     }
 
     @Override
     public List<String> getFullNames(List<Student> students) {
-        return students.stream()
-                .map(s -> s.getFirstName() + " " + s.getLastName())
-                .collect(Collectors.toList());
+        return (List<String>) getMappedCollection(students, s -> s.getFirstName() + " " + s.getLastName());
     }
 
     @Override
     public Set<String> getDistinctFirstNames(List<Student> students) {
-        return students.stream()
-                .map(Student::getFirstName)
-                .collect(Collectors.toSet());
+        return new HashSet<>(getMappedCollection(students, Student::getFirstName));
     }
 
     @Override
     public String getMinStudentFirstName(List<Student> students) {
         return sortStudentsById(students).stream()
-                .findFirst()
                 .map(Student::getFirstName)
+                .findFirst()
                 .orElse("");
     }
 
     @Override
     public List<Student> sortStudentsById(Collection<Student> students) {
-        return students.stream().
-                sorted(Comparator.comparingInt(Student::getId))
-                .collect(Collectors.toList());
+        return (List<Student>) getSortedCollection(students, Comparator.comparingInt(Student::getId));
     }
 
     @Override
     public List<Student> sortStudentsByName(Collection<Student> students) {
-        return students.stream()
-                .sorted(STUDENT_COMPARATOR)
-                .collect(Collectors.toList());
+        return (List<Student>) getSortedCollection(students, STUDENT_COMPARATOR);
     }
 
     @Override
     public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
-        return sortStudentsByName(
-                students.stream()
-                        .filter(s -> s.getFirstName().equals(name))
-                        .collect(Collectors.toList()));
+        return sortStudentsByName(getFilteredCollection(students, s -> s.getFirstName().equals(name)));
     }
 
     @Override
     public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
-        return sortStudentsByName(
-                students.stream()
-                        .filter(s -> s.getLastName().equals(name))
-                        .collect(Collectors.toList()));
+        return sortStudentsByName(getFilteredCollection(students, s -> s.getLastName().equals(name)));
     }
 
     @Override
     public List<Student> findStudentsByGroup(Collection<Student> students, String group) {
-        return sortStudentsByName(
-                students.stream()
-                        .filter(s -> s.getGroup().equals(group))
-                        .collect(Collectors.toList()));
+        return sortStudentsByName(getFilteredCollection(students, s -> s.getGroup().equals(group)));
     }
 
 
@@ -116,23 +91,22 @@ public class StudentQueryImpl implements StudentQuery {
     };
 
 
-    private <T, A, R> Collection<R> getMappedCollection(Collection<T> collection, Function<T, R> function, Collector<R, A, R> collectTo) {
-        return (Collection<R>) collection.stream()
+    private <T, R> Collection<R> getMappedCollection(Collection<T> collection, Function<T, R> function) {
+        return collection.stream()
                 .map(function)
-                .collect(collectTo);
+                .collect(Collectors.toList());
     }
 
-    private <T> Collection<T> getFilteredCollection(Collection<T> collection, Predicate<T> predicate, Collector collectTo) {
-        return (Collection<T>) collection.stream()
+    private <T> Collection<T> getFilteredCollection(Collection<T> collection, Predicate<T> predicate) {
+        return collection.stream()
                 .filter(predicate)
-                .collect(collectTo);
+                .collect(Collectors.toList());
     }
 
-    private <T> Collection<T> getSortedCollection(Collection<T> collection, Comparator<T> comparator, Collector collectTo) {
-        return (Collection<T>) collection.stream()
+    private <T> Collection<T> getSortedCollection(Collection<T> collection, Comparator<T> comparator) {
+        return collection.stream()
                 .sorted(comparator)
-                .collect(collectTo);
+                .collect(Collectors.toList());
     }
-
 
 }
