73b75a8d8144bc3eba49061185b3af86683fec8e
be39na <61196684+be39na@users.noreply.github.com>
2020-04-17 00:08:27 +0300

    implementation of FileEncodingReader and FileEncodingWriter

--- src/main/java/impl/file/FileEncodingWriterImpl.java
+++ src/main/java/impl/file/FileEncodingWriterImpl.java
@@ -1,4 +1,37 @@
 package impl.file;
 
-public class FileEncodingWriterImpl {
+import api.file.FileEncodingWriter;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class FileEncodingWriterImpl implements FileEncodingWriter {
+
+    @Override
+    public void write(File file, InputStream data, Charset dataEncoding) {
+        write(file, data, dataEncoding, StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public void write(File file, InputStream data, Charset dataEncoding, Charset fileEncoding) {
+
+        try(Reader input =new BufferedReader(new InputStreamReader(data,dataEncoding)))
+        {
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+            try (Writer output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), fileEncoding))) {
+                input.transferTo(output);
+            }
+
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(String.format("error in input stream %s",e.getMessage()));
+        }
+
+    }
+
 }
