81baa2a25770f5b2a29b8b12475d12e3f60cec76
MaxxKut <maksim.kutimskiy@gmail.com>
2020-04-16 21:13:52 +0300

    Task 4. implement FileEncodingReaderImpl, FileEncodingWriterImpl

--- src/main/java/impl/file/FileEncodingReaderFactory.java
+++ src/main/java/impl/file/FileEncodingReaderFactory.java
@@ -1,9 +1,10 @@
 package impl.file;
 
 import api.file.FileEncodingReader;
+import api.file.FileEncodingReaderImpl;
 
 public class FileEncodingReaderFactory {
     public static FileEncodingReader getInstance() {
-        return null;
+        return new FileEncodingReaderImpl();
     }
 }
