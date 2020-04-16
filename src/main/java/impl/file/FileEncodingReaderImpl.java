b4c8113087cb59553e93bd466ada7dd7bf6a929a
Alex Zhurba <alex7ey1@gmail.com>
2020-04-16 22:51:50 +0300

    Introduce implementation of FileEncodingReader

---
+++ src/main/java/impl/file/FileEncodingReaderImpl.java
@@ -0,0 +1,18 @@
+package impl.file;
+
+import api.file.FileEncodingReader;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
+public class FileEncodingReaderImpl implements FileEncodingReader {
+    @Override
+    public Reader read(File file, Charset fileEncoding) {
+        try {
+            return new BufferedReader(new FileReader(file, fileEncoding));
+        } catch (IOException e) {
+            System.out.println("Creating file reader error");
+        }
+        return null;
+    }
+}
