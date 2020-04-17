79772c90304eb3448ca8866265cda69e5094ed6f
lennuz17 <lennuz17@gmail.com>
2020-04-17 03:16:02 +0300

    hw 7

--- src/main/java/info/kgeorgiy/java/advanced/student/StudentQueryFactory.java
+++ src/main/java/info/kgeorgiy/java/advanced/student/StudentQueryFactory.java
@@ -3,7 +3,7 @@ package info.kgeorgiy.java.advanced.student;
 import java.util.function.Function;
 
 public class StudentQueryFactory {
-    public static StudentQuery getInstance() {
+    public static StudentQuery getInstance() { 
         return new StudentQueryImpl();
     }
 }
