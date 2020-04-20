5baf5223dead1d03cb4b1f449916c041ed6cbb77
lennuz17 <lennuz17@gmail.com>
2020-04-20 13:39:33 +0300

    cut anonymous class in separate class

---
+++ src/main/java/impl/file/FileEncodingReaderImpl.java
@@ -0,0 +1,23 @@
+package impl.file;
+
+import api.file.FileEncodingReader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
+public class FileEncodingReaderImpl implements FileEncodingReader {
+
+    @Override
+    public Reader read(File file, Charset fileEncoding) {
+        Reader reader = null;
+        try {
+            reader = new FileReader(file, fileEncoding);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return reader;
+    }
+}
