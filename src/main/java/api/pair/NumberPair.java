460fb28a9d13445647209e3cbecff608698e07ef
rznov <rznnova@gmail.com>
2020-04-21 14:08:46 +1100

    Implement Pair and NumberPair

--- src/main/java/api/pair/NumberPair.java
+++ src/main/java/api/pair/NumberPair.java
@@ -1,4 +1,4 @@
 package api.pair;
 
-public interface NumberPair<T extends Number, K extends Number> extends Pair<T, K> {
+public interface NumberPair<K extends Number, T extends Number> extends Pair<K, T> {
 }
