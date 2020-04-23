c5ddc8522f1d9d430709f33c205af1fbe4992551
Dmitry <DiamindFaceit@yandex.ru>
2020-04-23 21:07:44 +0300

    [HW_6] add PairImpl and NumberPairImpl

---
+++ src/main/java/factory/pair/PairImpl.java
@@ -0,0 +1,51 @@
+package factory.pair;
+
+import api.pair.Pair;
+import java.util.Objects;
+
+public class PairImpl<T, K> implements Pair<T, K> {
+
+  private final T first;
+  private final K second;
+
+  public PairImpl(T first, K second) {
+    this.first = first;
+    this.second = second;
+  }
+
+  @Override
+  public T getFirst() {
+    return first;
+  }
+
+  @Override
+  public K getSecond() {
+    return second;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PairImpl<?, ?> pair = (PairImpl<?, ?>) o;
+    return Objects.equals(first, pair.first) &&
+        Objects.equals(second, pair.second);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(first, second);
+  }
+
+  @Override
+  public String toString() {
+    return "PairImpl{" +
+        "first=" + first +
+        ", second=" + second +
+        '}';
+  }
+}
--- src/main/java/impl/pair/PairFactory.java
+++ src/main/java/impl/pair/PairFactory.java
@@ -1,9 +1,11 @@
 package impl.pair;
 
 import api.pair.Pair;
+import factory.pair.PairImpl;
 
 public class PairFactory {
+
     public static <K, T> Pair<K, T> of(K first, T second) {
-        return null;
+        return new PairImpl<>(first, second);
     }
 }
