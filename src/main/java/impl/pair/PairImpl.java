b296b66f5f30df9d74bcf19a22d06f2efe2e799e
Andy1897 <34788382+Andy1897@users.noreply.github.com>
2020-04-23 22:50:54 +0300

    add implements NumberPair and Pair

---
+++ src/main/java/impl/pair/PairImpl.java
@@ -0,0 +1,45 @@
+package impl.pair;
+
+import api.pair.Pair;
+import java.util.Objects;
+
+public class PairImpl<T, K> implements Pair<T, K> {
+
+    private final T first;
+    private final K second;
+
+
+    PairImpl(T first, K second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public T getFirst() {
+        return first;
+    }
+
+    @Override
+    public K getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        PairImpl<?, ?> pair = (PairImpl<?, ?>) obj;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [" + first.toString() + "," + second.toString() + "]";
+    }
+
+}
