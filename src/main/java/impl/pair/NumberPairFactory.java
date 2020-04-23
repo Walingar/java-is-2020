201ba54d53a55883260929800db11c7543a9cfff
Dmitry <DiamindFaceit@yandex.ru>
2020-04-23 21:07:44 +0300

    [HW_6] add PairImpl and NumberPairImpl

--- src/main/java/impl/pair/NumberPairFactory.java
+++ src/main/java/impl/pair/NumberPairFactory.java
@@ -1,9 +1,11 @@
 package impl.pair;
 
 import api.pair.Pair;
+import factory.pair.NumberPairImpl;
 
 public class NumberPairFactory {
+
     public static <K extends Number, T extends Number> Pair<K, T> of(K first, T second) {
-        return null;
+        return new NumberPairImpl<>(first, second);
     }
 }
