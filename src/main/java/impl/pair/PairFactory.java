22305ffb9408425d96df681f61bf2b752ced6f69
NeHo4y <whideofficial@gmail.com>
2020-04-23 15:58:50 +0300

    [task-6] Implement Pair, NumberPair

--- src/main/java/impl/pair/PairFactory.java
+++ src/main/java/impl/pair/PairFactory.java
@@ -1,9 +1,9 @@
 package impl.pair;
 
-import api.pair.Pair;
+        import api.pair.Pair;
 
 public class PairFactory {
     public static <K, T> Pair<K, T> of(K first, T second) {
-        return null;
+        return new PairImpl<>(first, second);
     }
 }
