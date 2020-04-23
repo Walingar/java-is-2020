8766db55d0836f2b074d5cb97810bf22a1c1195e
Elizaveta Leonova <elizaveta.l.1998@mail.ru>
2020-04-23 13:34:50 +0300

    Introduce implementations of NumberPair and Pair

--- src/main/java/impl/pair/PairFactory.java
+++ src/main/java/impl/pair/PairFactory.java
@@ -4,6 +4,8 @@ import api.pair.Pair;
 
 public class PairFactory {
     public static <K, T> Pair<K, T> of(K first, T second) {
-        return null;
+        return new PairImpl<>(first, second);
     }
+
+
 }
---
+++ src/main/java/impl/pair/PairImpl.java
@@ -0,0 +1,40 @@
+package impl.pair;
+
+import api.pair.Pair;
+
+import java.util.Objects;
+
+public class PairImpl<K, T> implements Pair<K, T> {
+    private final K first;
+    private final T second;
+
+    PairImpl(K first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public K getFirst() {
+        return first;
+    }
+
+    @Override
+    public T getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof PairImpl)) return false;
+        PairImpl<?, ?> pair = (PairImpl<?, ?>) obj;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair [" + first.toString() + "," + second.toString() + "]";
+    }
+
+}
