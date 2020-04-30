c050a74aa0c06a9eb21f11d2b434afecc3652fc2
Elizaveta Leonova <elizaveta.l.1998@mail.ru>
2020-04-30 13:36:54 +0300

    Introduce implementation of StudentQuery

---
+++ src/main/java/info/kgeorgiy/java/advanced/student/StudentDB.java
@@ -0,0 +1,100 @@
+package info.kgeorgiy.java.advanced.student;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
+
+public class StudentDB implements StudentQuery {
+    @Override
+    public List<String> getFirstNames(List<Student> students) {
+        return students.stream()
+                .map(Student::getFirstName)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> getLastNames(List<Student> students) {
+        return students.stream()
+                .map(Student::getLastName)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> getGroups(List<Student> students) {
+        return students.stream()
+                .map(Student::getGroup)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> getFullNames(List<Student> students) {
+        return students.stream()
+                .map(student -> student.getFirstName() + " " + student.getLastName())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Set<String> getDistinctFirstNames(List<Student> students) {
+        return students.stream()
+                .map(Student::getFirstName)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getMinStudentFirstName(List<Student> students) {
+        return students.stream()
+                .min(Student::compareTo)
+                .map(Student::getFirstName)
+                .orElse("");
+    }
+
+    @Override
+    public List<Student> sortStudentsById(Collection<Student> students) {
+        return students.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Student> sortStudentsByName(Collection<Student> students) {
+        return students.stream()
+                .sorted(STUDENT_COMPARATOR)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
+        return students.stream()
+                .filter(student -> student.getFirstName().equals(name))
+                .sorted(STUDENT_COMPARATOR)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
+        return students.stream()
+                .filter(student -> student.getLastName().equals(name))
+                .sorted(STUDENT_COMPARATOR)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Student> findStudentsByGroup(Collection<Student> students, String group) {
+        return students.stream()
+                .filter(student -> student.getGroup().equals(group))
+                .sorted(STUDENT_COMPARATOR)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Map<String, String> findStudentNamesByGroup(Collection<Student> students, String group) {
+        return students.stream()
+                .filter(student -> student.getGroup().equals(group))
+                .collect(toMap(Student::getLastName, Student::getFirstName, (a, b) -> a.compareTo(b) < 0 ? a : b));
+    }
+
+    private static final Comparator<? super Student> STUDENT_COMPARATOR = Comparator.comparing(Student::getLastName)
+            .thenComparing(Student::getFirstName)
+            .thenComparing(Student::getId);
+}
