252304ad5d971cda4378371269077ba9d42fae07
be39na <61196684+be39na@users.noreply.github.com>
2020-04-30 23:03:40 +0300

    Implementation studentDB

--- src/main/java/info/kgeorgiy/java/advanced/student/StudentDB.java
+++ src/main/java/info/kgeorgiy/java/advanced/student/StudentDB.java
@@ -1,4 +1,112 @@
 package info.kgeorgiy.java.advanced.student;
 
-public class StudentDB {
+import java.util.*;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class StudentDB implements  StudentQuery {
+
+
+    @Override
+    public List<String> getFirstNames(List<Student> students) {
+        return getValueList(students,Student::getFirstName);
+    }
+
+    @Override
+    public List<String> getLastNames(List<Student> students) {
+        return getValueList(students,Student::getLastName);
+    }
+
+    @Override
+    public List<String> getGroups(List<Student> students) {
+        return getValueList(students,Student::getGroup);
+    }
+
+
+    @Override
+    public List<String> getFullNames(List<Student> students) {
+        return getValueList(students, student -> student.getFirstName() + " " + student.getLastName());
+    }
+
+    @Override
+    public Set<String> getDistinctFirstNames(List<Student> students)
+    {
+        return new HashSet<>(getValueList(students,Student::getFirstName));
+    }
+
+    @Override
+    public String getMinStudentFirstName(List<Student> students)
+    {
+        if( getSortedSubSet(students, Comparator.comparingInt(Student::getId)).isEmpty()) return  "";
+      return getSortedSubSet(students, Comparator.comparingInt(Student::getId)).get(0).getFirstName();
+    }
+
+    @Override
+    public List<Student> sortStudentsById (Collection<Student> students)
+    {
+        return   getSortedSubSet((List<Student>) students, Comparator.comparingInt(Student::getId));
+    }
+
+    @Override
+    public List<Student> sortStudentsByName(Collection<Student> students)
+    {
+        return   getSortedSubSet((List<Student>) students, Comparator.comparing(Student::getLastName)
+                .thenComparing(Student::getFirstName)
+                .thenComparing(Student::getId));
+    }
+
+    @Override
+    public List<Student> findStudentsByFirstName(Collection<Student> students, String name)
+    {
+        return sortStudentsByName(getSubList((List<Student>) students, s -> s.getFirstName().equals(name)));
+    }
+
+    @Override
+    public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
+        return sortStudentsByName(getSubList((List<Student>) students, s -> s.getLastName().equals(name)));
+    }
+
+    @Override
+    public List<Student> findStudentsByGroup(Collection<Student> students, String group) {
+        return sortStudentsByName(getSubList((List<Student>) students, s -> s.getGroup().equals(group)));
+    }
+
+    @Override
+    public Map<String, String> findStudentNamesByGroup(Collection<Student> students, String group) {
+        return findStudentsByGroup(students, group).stream()
+                .collect(Collectors.toMap(
+                        Student::getLastName, Student::getFirstName, BinaryOperator.minBy(Comparator.naturalOrder())
+                        )
+                );
+    }
+
+    private List<String> getValueList(List<Student> students, Function<Student, String> function)
+    {
+        return  students.stream()
+                .map(function)
+                .collect(Collectors.toList());
+    }
+
+    private List<Student> getSubList(List<Student> students, Predicate<Student> predicate)
+    {
+        return students.stream()
+                .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    private List<Student> getSortedSubSet (List<Student> students, Comparator<Student> comparator) {
+        return students.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
+
+
+
 }
