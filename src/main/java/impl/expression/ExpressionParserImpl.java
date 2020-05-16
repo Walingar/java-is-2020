32dbb2b238e2f457092a6584d6dfd8da9ddbd897
MaxxKut <maksim.kutimskiy@gmail.com>
2020-05-16 17:11:50 +0300

    Task 2. fix code review

--- src/main/java/impl/expression/ExpressionParserImpl.java
+++ src/main/java/impl/expression/ExpressionParserImpl.java
@@ -10,21 +10,24 @@ public class ExpressionParserImpl implements ExpressionParser {
         if (expression == null) {
             throw new IllegalArgumentException("Expression is null");
         }
-        expression = expression.replaceAll("\\s", "");
         if (expression.isBlank()) {
             throw new IllegalArgumentException("Expression is empty");
         }
         int result = 0;
         StringBuilder currentNumber = new StringBuilder();
-        //проход по строке
+        // parsing string
         for (int index = 0; index < expression.length(); index++) {
             char currentChar = expression.charAt(index);
-            // Дописываем цифру в строку
+            // if whitespace
+            if(Character.isWhitespace(currentChar)){
+                continue;
+            }
+            // adding digit
             if (Character.isDigit(currentChar)) {
                 currentNumber.append(currentChar);
                 continue;
             }
-            // если знак
+            // sign processing
             if (currentChar == '+' || currentChar == '-') {
                 if(currentNumber.length() != 0){
                     result = Math.addExact(result, getNumber(currentNumber));
