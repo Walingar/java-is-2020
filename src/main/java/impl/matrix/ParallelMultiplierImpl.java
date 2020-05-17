6d80a180215746df8d607b6c93aa925349ca7581
Alexander Sukhikh <xbox24_772@mail.ru>
2020-05-18 00:24:02 +0300

    Code reformatted.

--- src/main/java/impl/matrix/ParallelMultiplierImpl.java
+++ src/main/java/impl/matrix/ParallelMultiplierImpl.java
@@ -6,7 +6,7 @@ public class ParallelMultiplierImpl implements ParallelMultiplier {
 
     private final int maxThreadCount;
 
-    ParallelMultiplierImpl(int maxThreadCount){
+    ParallelMultiplierImpl(int maxThreadCount) {
         this.maxThreadCount = maxThreadCount;
     }
 
@@ -15,10 +15,10 @@ public class ParallelMultiplierImpl implements ParallelMultiplier {
 
         double[][] result = new double[a.length][b[0].length];
         int operationsCnt = a.length * b[0].length;
-        int partSize = operationsCnt/maxThreadCount;
-        Thread[] threads = new Thread[maxThreadCount-1];
+        int partSize = operationsCnt / maxThreadCount;
+        Thread[] threads = new Thread[maxThreadCount - 1];
 
-        for(int i = 0; i < threads.length; i++){
+        for (int i = 0; i < threads.length; i++) {
             int partBegin = i * partSize;
             int partEnd = partBegin + partSize;
             threads[i] = new Thread(() -> calulatePart(a, b, result, partBegin, partEnd));
@@ -27,23 +27,23 @@ public class ParallelMultiplierImpl implements ParallelMultiplier {
 
         calulatePart(a, b, result, threads.length * partSize, operationsCnt);
 
-        for(int i = 0; i < threads.length; i++){
+        for (int i = 0; i < threads.length; i++) {
             try {
                 threads[i].join();
-            }catch (InterruptedException ignored) {
+            } catch (InterruptedException ignored) {
             }
         }
 
         return result;
     }
 
-    private void calulatePart(double[][] a, double[][] b, double[][] res, int partBegin, int partEnd){
+    private void calulatePart(double[][] a, double[][] b, double[][] res, int partBegin, int partEnd) {
 
         int rowSize = res[0].length;
-        for(int element = partBegin; element < partEnd; element++){
+        for (int element = partBegin; element < partEnd; element++) {
             int i = element / rowSize;
             int j = element % rowSize;
-            for(int k = 0; k < b.length; k++){
+            for (int k = 0; k < b.length; k++) {
                 res[i][j] += a[i][k] * b[k][j];
             }
         }
