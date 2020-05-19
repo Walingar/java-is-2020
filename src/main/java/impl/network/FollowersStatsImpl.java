4b1cc850f9537f29d4643f6a57ae99a610515a61
Mike Saravayskiy <maiksaray@gmail.com>
2020-05-20 02:24:31 +0300

    OK, I'm completely broken by all these thenCompose/Combine. I've spent 4hrs trying to understand how can I return Future value without get() from inside thenCombine()! LITERALLY FOUR HOURS.
    But in the end I think this looks nice =)
    
    Maybe you get used to these once you've spent some time getting into all of this...

--- src/main/java/impl/network/FollowersStatsImpl.java
+++ src/main/java/impl/network/FollowersStatsImpl.java
@@ -4,15 +4,11 @@ import api.network.FollowersStats;
 import api.network.SocialNetwork;
 import api.network.UserInfo;
 
-import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.Predicate;
 
 public class FollowersStatsImpl implements FollowersStats {
     private SocialNetwork network;
-
-    private Map<Integer, UserInfo> userInfoCache = new ConcurrentHashMap<>();
-    private Map<Integer, Collection<Integer>> followersCache = new ConcurrentHashMap<>();
 
     public FollowersStatsImpl(SocialNetwork network) {
         this.network = network;
@@ -20,67 +16,33 @@ public class FollowersStatsImpl implements FollowersStats {
 
     @Override
     public Future<Integer> followersCountBy(int id, int depth, Predicate<UserInfo> predicate) {
-        return CompletableFuture.completedFuture(
-                doCountFollowersBy(id, depth, predicate,
-                        new ConcurrentHashMap<>() {{
-                            put(id, id);
-                        }}
-                ));
+        return doCountFollowersBy(id, depth, predicate,
+                new ConcurrentHashMap<>() {{
+                    put(id, id);
+                }}
+        );
     }
 
-    private Integer doCountFollowersBy(int id, int depth, Predicate<UserInfo> predicate, ConcurrentHashMap<Integer, Integer> visited) {
-        try {
-            var userInfo = getUserInfo(id);
-
-            var thisUserValue = predicate.test(userInfo) ? 1 : 0;
-
-            if (depth == 0) {
-                return thisUserValue;
-            }
-
-            var followersFutures = new ArrayList<Future<Integer>>();
-            for (var followerId : getUserFollowers(id)) {
-
-                if (visited.putIfAbsent(followerId, followerId) != null) {
-                    continue;
-                }
-                followersFutures.add(
-                        CompletableFuture.supplyAsync(() -> doCountFollowersBy(followerId, depth - 1, predicate, visited))
+    private CompletableFuture<Integer> doCountFollowersBy(int id, int depth, Predicate<UserInfo> predicate, ConcurrentHashMap<Integer, Integer> visited) {
+        var userCountFuture = network.getUserInfo(id)
+                .thenApply(
+                        (userInfo) -> predicate.test(userInfo) ? 1 : 0
                 );
-            }
+        if (depth == 0) return userCountFuture;
 
-            for (var future : followersFutures) {
-                thisUserValue += future.get();
-            }
-
-            return thisUserValue;
-        } catch (InterruptedException | ExecutionException e) {
-            System.err.println(String.format("Something went wrong: %s", e.getMessage()));
-            return null;
-        }
+        return network.getFollowers(id)
+                .thenCompose(
+                        (followers) ->
+                                followers.stream()
+                                        .map(
+                                                follower -> {
+                                                    if (visited.putIfAbsent(follower, follower) != null) {
+                                                        return CompletableFuture.completedFuture(0);
+                                                    }
+                                                    return doCountFollowersBy(follower, depth - 1, predicate, visited);
+                                                })
+                                        .reduce((l, r) -> l.thenCombine(r, Integer::sum))
+                                        .orElse(CompletableFuture.completedFuture(0)))
+                .thenCombine(userCountFuture, Integer::sum);
     }
-
-    private UserInfo getUserInfo(int id) {
-        return userInfoCache.computeIfAbsent(id, (k) -> {
-            try {
-                return network.getUserInfo(id).get();
-            } catch (InterruptedException | ExecutionException e) {
-                System.err.println(String.format("Something went wrong: %s", e.getMessage()));
-                return null; // this way we will not add anything to the map
-            }
-        });
-    }
-
-    private Collection<Integer> getUserFollowers(int id) {
-        return followersCache.computeIfAbsent(id,
-                (k) -> {
-                    try {
-                        return network.getFollowers(id).get();
-                    } catch (InterruptedException | ExecutionException e) {
-                        System.err.println(String.format("Something went wrong: %s", e.getMessage()));
-                        return null; // this way we will not add anything to the map
-                    }
-                });
-    }
-
 }
