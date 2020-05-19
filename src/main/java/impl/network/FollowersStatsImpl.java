a5b6b1a2bb354278aa265645cfddcb07fbc37d59
Mike Saravayskiy <maiksaray@gmail.com>
2020-05-19 20:07:34 +0300

    Polished the code, optimized computing so that we check id being visited only once as fast as possible. Still can't beat two threads test. There are some leftovers from another approach so that if I'm wrong with ConcurrentHashMap.putIfAbsent()==null I still have the solutions

--- src/main/java/impl/network/FollowersStatsImpl.java
+++ src/main/java/impl/network/FollowersStatsImpl.java
@@ -3,12 +3,8 @@ package impl.network;
 import api.network.FollowersStats;
 import api.network.SocialNetwork;
 import api.network.UserInfo;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
@@ -28,53 +24,84 @@ public class FollowersStatsImpl implements FollowersStats {
 
     @Override
     public Future<Integer> followersCountBy(int id, int depth, Predicate<UserInfo> predicate) {
-//        if (depth == 0) return CompletableFuture.completedFuture(0); //Wow, this is ugly
         try {
-            return doCountFollowersBY(id, depth, predicate, new ArrayList<>());
+            return doCountFollowersBy(id, depth, predicate,
+                    new ConcurrentHashMap<>() {{
+                        put(id, id);
+                    }}
+//                    new HashSet<>() {{  // WHY DOES Set.of(id) RETURN IMMUTABLE SET?!
+//                        add(id);        // This ends up being ugly... Stream.of().collectToSet is also ugly
+//                    }}
+            );
         } catch (InterruptedException | ExecutionException e) {
             System.err.println(String.format("Something went wrong during followers count (depth %d): %s", depth, e.getMessage()));
-            return null;
+            return CompletableFuture.completedFuture(0); //Or null, dunno, not specified =)
         }
     }
 
-    private Future<Integer> doCountFollowersBY(int id, int depth, Predicate<UserInfo> predicate, List<Integer> visited)
+    /**
+     * Recursively counts followers in a network that satisfy given predicate
+     *
+     * @param id        id of a user to start count followers
+     * @param depth     max depth of recursive travers
+     * @param predicate predicate to check
+     * @param visited   accumulator of already visited nodes so that we don't count the same user twice
+     */
+    private Future<Integer> doCountFollowersBy(int id, int depth, Predicate<UserInfo> predicate, ConcurrentHashMap<Integer, Integer> visited)//Set<Integer> visited)
             throws ExecutionException, InterruptedException {
-        var thisUserValue = 0;
-        synchronized (getCacheSyncObject(id)) {
-            if (!visited.contains(id)) { // Or we can do this in the loop below, dunno what is more performant, dunno what is safer
-                var userInfo = getUserInfo(id);
-                thisUserValue = predicate.test(userInfo) ? 1 : 0;
-                visited.add(id);
-            }
-        }
+
+        var userInfo = getUserInfo(id);
+
+        var thisUserValue = predicate.test(userInfo) ? 1 : 0;
+
         if (depth == 0) {
-            return CompletableFuture.completedFuture(thisUserValue);
+            return CompletableFuture.completedFuture(thisUserValue); //Wow, this is ugly
         }
 
         var followersFutures = new ArrayList<Future<Integer>>();
-        for (var followerID : getUserFollowers(id)) {
-            // We can double check if we already visited follower here
-            followersFutures.add(doCountFollowersBY(followerID, depth - 1, predicate, visited));
+        for (var followerId : getUserFollowers(id)) {
+
+//            This is a leftover from when visited was a Set
+//            synchronized (getCacheSyncObject(id)) {
+//                if (!visited.contains(followerId)) {
+//                    visited.add(followerId);
+//                } else {
+//                    continue;
+//                }
+//            }
+            // this is thread-safe without additional synchronization, right? right?
+            if (visited.putIfAbsent(followerId, followerId) != null) { //very readable tho...
+                continue;
+            }
+            followersFutures.add(doCountFollowersBy(followerId, depth - 1, predicate, visited));
         }
 
-        for (var future: followersFutures){
+        for (var future : followersFutures) {
             thisUserValue += future.get();
         }
-
 
         return CompletableFuture.completedFuture(thisUserValue);
     }
 
+    /**
+     * Returns object that can be used as synchronize object around cache access
+     * Ok, here for each id we try to get some object to synchronize around, we store those in separate concurrentMap
+     * We use (autoboxed)Integer as map key, but since checking for already being there is based on equals()
+     * we will not create separate locks for different boxed Integer instances.
+     * Here we store the very first autoboxed Integer as an object to sync around
+     * This is needed so that putIfAbsent is atomic and we don't create new objects per call:
+     * userIdLocks.putIfAbsent(id, new Object()) <- creates new Object before call even if we don't need it
+     * if(userIdLocks.containsKey(id) userIdLocks.put(id, new Object) <- not atomic
+     *
+     * @param id int id of synch object.
+     * @return Object (Integer really) unique object to synchronize around to access caches.
+     */
     private Object getCacheSyncObject(final Integer id) {
-//        Here we store the very first autoboxed Integer as an object to sync around
         userIdLocks.putIfAbsent(id, id);
         return userIdLocks.get(id);
     }
 
     private UserInfo getUserInfo(int id) throws ExecutionException, InterruptedException {
-        // Ok, here for each id we try to get some object to synchronize around, we store those in separate concurrentMap
-        // We use (autoboxed)Integer as map key, but since checking for already being there is based on equals()
-        //    we will not create separate locks for different boxed Integer instances.
         synchronized (getCacheSyncObject(id)) {
             if (!userInfoCache.containsKey(id)) {
                 userInfoCache.put(id, network.getUserInfo(id).get());
