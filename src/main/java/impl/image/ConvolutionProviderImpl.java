99dbb76eeb2a6545613bad23f9fc5540f0cffe23
Sergey Stepanov <stepanov.s@numdes.com>
2020-05-25 00:00:49 +0300

    update implementation

--- src/main/java/impl/image/ConvolutionProviderImpl.java
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -2,13 +2,15 @@ package impl.image;
 
 import api.image.ConvolutionProvider;
 
+import java.awt.*;
+
 public class ConvolutionProviderImpl implements ConvolutionProvider {
     @Override
     public Color[][] apply(Color[][] image, double[][] kernel) {
         int numRows = image.length;
         int numColumns = image[0].length;
 
-        var resultImage = new Color[numRows][]
+        var resultImage = new Color[numRows][];
 
         for (var rowIdx = 0; rowIdx < numRows; rowIdx++) {
             resultImage[rowIdx] = new Color[numColumns];
