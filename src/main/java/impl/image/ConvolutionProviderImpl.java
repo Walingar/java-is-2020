28ee210a81942ef637bae57076dd093366bf431e
Sergey Stepanov <stepanov.s@numdes.com>
2020-05-25 20:55:25 +0300

    add ConvolutionProvider implementation and ImageConverter implementation

---
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -0,0 +1,50 @@
+package impl.image;
+
+import api.image.ConvolutionProvider;
+
+import java.awt.*;
+
+public class ConvolutionProviderImpl implements ConvolutionProvider {
+    @Override
+    public Color[][] apply(Color[][] image, double[][] kernel) {
+        int numRows = image.length;
+        int numColumns = image[0].length;
+
+        var resultImage = new Color[numRows][];
+
+        for (var rowIdx = 0; rowIdx < numRows; rowIdx++) {
+            resultImage[rowIdx] = new Color[numColumns];
+            for (var colIdx = 0; colIdx < numColumns; colIdx++)
+                resultImage[rowIdx][colIdx] = applyKernelToPixel(image, kernel, rowIdx, colIdx);
+        }
+
+        return resultImage;
+    }
+
+    private Color applyKernelToPixel(Color[][] image, double[][] kernel, int pixelRowIdx, int pixelColIdx) {
+        int numRows = image.length;
+        int numColumns = image[0].length;
+
+        int kernelShiftRow = kernel.length / 2;
+        int kernelShiftColumn = kernel[0].length / 2;
+
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+
+        for (var rowShift = -kernelShiftRow; rowShift <= kernelShiftRow; rowShift++) {
+            for (var colShift = -kernelShiftColumn; colShift <= kernelShiftColumn; colShift++) {
+                int rowIdx = pixelRowIdx + rowShift;
+                int colIdx = pixelColIdx + colShift;
+                if (rowIdx >= 0 && rowIdx < numRows && colIdx >= 0 && colIdx < numColumns) {
+                    double kernelPixel = kernel[kernelShiftRow + rowShift][kernelShiftColumn + colShift];
+                    Color imagePixelColor = image[rowIdx][colIdx];
+                    red += imagePixelColor.getRed() * kernelPixel;
+                    green += imagePixelColor.getGreen() * kernelPixel;
+                    blue += imagePixelColor.getBlue() * kernelPixel;
+                }
+            }
+        }
+        return new Color(red, green, blue);
+    }
+}
