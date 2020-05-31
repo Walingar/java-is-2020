81f504bcd27142c4088355a465e441345e52d0db
Sergey Stepanov <stepanov.s@numdes.com>
2020-05-31 18:34:55 +0300

    update YearTemperatureStatsParserImpl

--- src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
@@ -8,6 +8,24 @@ import java.util.Collection;
 public class YearTemperatureStatsParserImpl implements YearTemperatureStatsParser {
     @Override
     public YearTemperatureStats parse(Collection<String> rawData) {
-        return null;
+        if (rawData == null) {
+            throw new IllegalArgumentException("`rawData` must be not null");
+        }
+
+        var yearTemperatureStats = YearTemperatureStatsFactory.getInstance();
+
+        for (var data : rawData) {
+            String[] tokens = data.split("[. ]");
+            int day = Integer.parseInt(tokens[0]);
+            int month = Integer.parseInt(tokens[1]);
+            int temperature = Integer.parseInt(tokens[2]);
+            yearTemperatureStats.updateStats(new DayTemperatureInfoImpl(
+                    day,
+                    month,
+                    temperature
+            ));
+        }
+
+        return yearTemperatureStats;
     }
 }
