487255f758ce8288c84ceeb7a775b97ed64c8d8f
Alexey <alexeyyushchuk@rambler.com>
2020-05-31 21:49:05 +0300

    Queues implementations

---
+++ src/main/java/queue/impl/ArrayQueueImpl.java
@@ -0,0 +1,142 @@
+package queue.impl;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class ArrayQueueImpl extends AbstractQueue<Integer> {
+    public Integer[] elements;
+
+    public int firstElementIndex;
+    public int lastElementIndex;
+
+    public int capacity;
+
+    private static final int DEFAULT_CAPACITY = 4;
+    private static final int CAPACITY_MULTIPLIER = 2;
+
+    public ArrayQueueImpl() {
+        elements = new Integer[DEFAULT_CAPACITY];
+        capacity = DEFAULT_CAPACITY;
+
+        firstElementIndex = -1;
+        lastElementIndex = -1;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            private int currentPosition = firstElementIndex;
+
+            @Override
+            public boolean hasNext() {
+                return !(currentPosition == lastElementIndex);
+            }
+
+            public Integer next() {
+                Integer elementToReturn = elements[currentPosition];
+                currentPosition++;
+                if (currentPosition != lastElementIndex && currentPosition >= capacity)
+                    currentPosition = 0;
+
+                return elementToReturn;
+            }
+        };
+    }
+
+    @Override
+    public int size() {
+        if (firstElementIndex == -1)
+            return 0;
+
+        if (lastElementIndex >= firstElementIndex)
+            return (lastElementIndex - firstElementIndex + 1);
+
+        return (firstElementIndex - lastElementIndex + 1);
+    }
+
+
+    @Override
+    public boolean offer(Integer integer) {
+        if (firstElementIndex == -1) {
+            firstElementIndex = 0;
+            lastElementIndex = 0;
+
+            elements[firstElementIndex] = integer;
+        } else {
+            boolean exceedsLinearCapacity = (lastElementIndex + 1) >= capacity;
+            boolean exceedsCyclicCapacity = (lastElementIndex < firstElementIndex) &&
+                    ((lastElementIndex + 1) >= firstElementIndex);
+
+            if (exceedsLinearCapacity || exceedsCyclicCapacity) {
+                capacity = capacity * CAPACITY_MULTIPLIER;
+                Integer[] newArray = new Integer[capacity];
+
+                if (exceedsLinearCapacity) {
+                    if (lastElementIndex + 1 - firstElementIndex >= 0)
+                        System.arraycopy(elements, firstElementIndex, newArray, 0, lastElementIndex + 1 - firstElementIndex);
+
+                    lastElementIndex = lastElementIndex - firstElementIndex;
+                    firstElementIndex = 0;
+                } else {
+                    if (firstElementIndex + 1 - lastElementIndex >= 0)
+                        System.arraycopy(elements, lastElementIndex, newArray, 0, firstElementIndex + 1 - lastElementIndex);
+
+                    firstElementIndex = firstElementIndex - lastElementIndex;
+                    lastElementIndex = 0;
+
+                    //Reverse array for linear state
+                    Integer temporary;
+                    for (int index = 0; index <= firstElementIndex; index++) {
+                        temporary = newArray[firstElementIndex - index];
+                        newArray[firstElementIndex - index] = newArray[index];
+                        newArray[index] = temporary;
+                    }
+
+                    lastElementIndex = firstElementIndex;
+                    firstElementIndex = 0;
+                }
+
+                elements = newArray;
+            }
+
+            lastElementIndex++;
+
+            elements[lastElementIndex] = integer;
+        }
+
+        return true;
+    }
+
+    @Override
+    public Integer poll() {
+        if (firstElementIndex == -1)
+            return null;
+
+        Integer elementToReturn = elements[firstElementIndex];
+
+        if (lastElementIndex < firstElementIndex) {
+            firstElementIndex++;
+
+            if (firstElementIndex >= capacity)
+                firstElementIndex = 0;
+        } else {
+            firstElementIndex++;
+            if (firstElementIndex > lastElementIndex) {
+                firstElementIndex = -1;
+                lastElementIndex = -1;
+            }
+        }
+
+        return elementToReturn;
+    }
+
+    @Override
+    public Integer peek() {
+        if (firstElementIndex == -1)
+            return null;
+
+        return elements[firstElementIndex];
+    }
+}
---
+++ src/main/java/queue/impl/LinkedQueueImpl.java
@@ -0,0 +1,85 @@
+package queue.impl;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+
+public class LinkedQueueImpl extends AbstractQueue<Integer> {
+    private QueueNodeImpl firstElement;
+    private QueueNodeImpl lastElement;
+
+    private int currentSize;
+
+    public LinkedQueueImpl() {
+        firstElement = null;
+        lastElement = null;
+        currentSize = 0;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<>() {
+            private QueueNodeImpl currentNode = firstElement;
+
+            @Override
+            public boolean hasNext() {
+                return currentNode != lastElement;
+            }
+
+            @Override
+            public Integer next() {
+                Integer valueToReturn = currentNode.getValue();
+                currentNode = currentNode.getNext();
+
+                return valueToReturn;
+            }
+        };
+    }
+
+    @Override
+    public int size() {
+        return currentSize;
+    }
+
+    @Override
+    public boolean offer(Integer integer) {
+        QueueNodeImpl newNode = new QueueNodeImpl(integer);
+        if (firstElement == null) {
+            firstElement = newNode;
+            lastElement = newNode;
+            lastElement.setPrevious(firstElement);
+        } else {
+            newNode.setPrevious(lastElement);
+
+            lastElement.setNext(newNode);
+            lastElement = newNode;
+        }
+
+        currentSize++;
+
+        return true;
+    }
+
+    @Override
+    public Integer poll() {
+        if (currentSize == 0)
+            return null;
+
+        Integer valueToReturn = firstElement.getValue();
+        firstElement = firstElement.getNext();
+
+        currentSize--;
+
+        return valueToReturn;
+    }
+
+    @Override
+    public Integer peek() {
+        if (currentSize == 0)
+            return null;
+
+        return firstElement.getValue();
+    }
+}
