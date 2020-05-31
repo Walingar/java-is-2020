929dd2038b5f8456cc703da343a2f5833b5ca14e
be39na <61196684+be39na@users.noreply.github.com>
2020-05-31 18:43:53 +0300

    Review fixes

--- src/main/java/impl/weather/DayTemperatureInfoImpl.java
+++ src/main/java/impl/weather/DayTemperatureInfoImpl.java
@@ -4,30 +4,30 @@ import java.time.Month;
 
 import api.weather.DayTemperatureInfo;
 
-class DayTemperatureInfoImpl implements DayTemperatureInfo {
-    private int _day;
-    private Month _month;
-    private int _temperature;
+final class DayTemperatureInfoImpl implements DayTemperatureInfo {
+    private int day;
+    private Month month;
+    private int temperature;
 
     DayTemperatureInfoImpl(int day, Month month, int temperature) {
-        this._day = day;
-        this._month = month;
-        this._temperature = temperature;
+        this.day = day;
+        this.month = month;
+        this.temperature = temperature;
     }
 
     @Override
     public int getDay() {
-        return _day;
+        return day;
     }
 
     @Override
     public Month getMonth() {
-        return _month;
+        return month;
     }
 
     @Override
     public int getTemperature() {
-        return _temperature;
+        return temperature;
     }
 
 }
