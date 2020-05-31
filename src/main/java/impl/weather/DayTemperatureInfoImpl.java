164ebd247128e62fcae50b8b889eb7459e55f68e
Sergey Stepanov <stepanov.s@numdes.com>
2020-05-31 17:02:25 +0300

    add frame

---
+++ src/main/java/impl/weather/DayTemperatureInfoImpl.java
@@ -0,0 +1,32 @@
+package impl.weather;
+
+import api.weather.DayTemperatureInfo;
+
+import java.time.Month;
+
+public class DayTemperatureInfoImpl implements DayTemperatureInfo {
+    private final int day;
+    private final Month month;
+    private final int temperature;
+
+    public DayTemperatureInfoImpl(int day, int month, int temperature) {
+        this.day = day;
+        this.month = Month.of(month);
+        this.temperature = temperature;
+    }
+
+    @Override
+    public int getDay() {
+        return day;
+    };
+
+    @Override
+    public Month getMonth() {
+        return month;
+    };
+
+    @Override
+    public int getTemperature() {
+        return temperature;
+    };
+}
---
+++ src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
@@ -0,0 +1,13 @@
+package impl.weather;
+
+import api.weather.YearTemperatureStats;
+import api.weather.YearTemperatureStatsParser;
+
+import java.util.Collection;
+
+public class YearTemperatureStatsParserImpl implements YearTemperatureStatsParser {
+    @Override
+    public YearTemperatureStats parse(Collection<String> rawData) {
+        return null;
+    }
+}
