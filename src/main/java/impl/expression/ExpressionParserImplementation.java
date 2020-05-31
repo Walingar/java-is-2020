8f49a00de0f292b9c4a7a42652f44be4d8779486
be39na <61196684+be39na@users.noreply.github.com>
2020-05-31 17:54:47 +0300

    Review fixes

--- src/main/java/impl/expression/ExpressionParserImplementation.java
+++ src/main/java/impl/expression/ExpressionParserImplementation.java
@@ -11,18 +11,34 @@ public class ExpressionParserImplementation implements ExpressionParser {
         if (expression == null) {
             throw new IllegalArgumentException("NULL expression");
         }
-        expression = expression.replaceAll("\\s", "");
+
 
         if (expression.isBlank()) {
             throw new IllegalArgumentException("Blank expression");
         }
 
+        if(!expression.contains("+")&&!expression.contains("-") )
+        {
+            try {
+                result = Math.addExact(result, Integer.parseInt(expression.toString()));
+                return  result;
+            }
+            catch (NumberFormatException a) {
+                throw new ParseException("Incorrect format");
+            }
+
+        }
         for (int i = 0; i < expression.length(); i++) {
             char bufSymbol = expression.charAt(i);
+
             if (Character.isDigit(bufSymbol)) {
                 bufString.append(bufSymbol);
             }
-            if (bufSymbol == '+' || bufSymbol == '-') {
+
+
+
+           //"1 + 12 + 3 + 564 + 123"
+            if (bufSymbol == '+' || bufSymbol == '-'||i==(expression.length()-1)) {
                 if (bufString.length() != 0) {
                     try {
                         result = Math.addExact(result, Integer.parseInt(bufString.toString()));
@@ -33,14 +49,7 @@ public class ExpressionParserImplementation implements ExpressionParser {
                 }
                 bufString.append(bufSymbol);
             }
-        }
 
-        if (bufString.length() != 0) {
-            try {
-                result = Math.addExact(result, Integer.parseInt(bufString.toString()));
-            } catch (NumberFormatException a) {
-                throw new ParseException("Incorrect format");
-            }
         }
         return result;
     }
