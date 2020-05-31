f33d39f6eb07639a3b93703329cb832c17b5e5f9
Sergey Stepanov <stepanov.s@numdes.com>
2020-05-31 19:00:27 +0300

    update implementation

--- src/main/java/impl/weather/YearTemperatureStatsImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsImpl.java
@@ -5,6 +5,7 @@ import api.weather.MonthTemperatureStats;
 import api.weather.YearTemperatureStats;
 
 import java.time.Month;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,32 +13,53 @@ import java.util.Map;
 public class YearTemperatureStatsImpl implements YearTemperatureStats {
     private Map<Month, MonthTemperatureStats> monthTemperatureStatsMap;
 
+    public YearTemperatureStatsImpl() {
+        monthTemperatureStatsMap = new HashMap<>();
+    }
+
     @Override
     public void updateStats(DayTemperatureInfo info) {
-        var monthTemperatureStats = monthTemperatureStatsMap.computeIfAbsent(
-                info.getMonth(), month -> new MonthTemperatureStatsImpl(info.getMonth()));
+        var month = info.getMonth();
+        if (!monthTemperatureStatsMap.containsKey(info.getMonth())) {
+            monthTemperatureStatsMap.put(month, new MonthTemperatureStatsImpl(month));
+        }
+        var monthTemperatureStats = monthTemperatureStatsMap.get(month);
         monthTemperatureStats.updateStats(info);
     }
 
     @Override
     public Double getAverageTemperature(Month month) {
-        return monthTemperatureStatsMap.get(month).getAverageTemperature();
+        if (monthTemperatureStatsMap.containsKey(month)) {
+            return monthTemperatureStatsMap.get(month).getAverageTemperature();
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Map<Month, Integer> getMaxTemperature() {
         Map<Month, Integer> maxTemperatureMonthMap = new HashMap<>();
-        maxTemperatureMonthMap.replaceAll((m, v) -> monthTemperatureStatsMap.get(m).getMaxTemperature());
+        for (var month : monthTemperatureStatsMap.keySet()) {
+            maxTemperatureMonthMap.put(month, monthTemperatureStatsMap.get(month).getMaxTemperature());
+        }
         return maxTemperatureMonthMap;
     }
 
     @Override
     public List<DayTemperatureInfo> getSortedTemperature(Month month) {
-        return monthTemperatureStatsMap.get(month).getSortedTemperature();
+        if (monthTemperatureStatsMap.containsKey(month)) {
+            return monthTemperatureStatsMap.get(month).getSortedTemperature();
+        } else {
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public DayTemperatureInfo getTemperature(int day, Month month) {
-        return monthTemperatureStatsMap.get(month).getTemperature(day);
+        if (monthTemperatureStatsMap.containsKey(month)) {
+            return monthTemperatureStatsMap.get(month).getTemperature(day);
+        } else {
+            return null;
+        }
     }
 }
