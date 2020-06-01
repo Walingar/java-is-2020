803278a4f85294806413e535cb6a7e7d84b89f5a
Sergey Stepanov <stepanov.s@numdes.com>
2020-06-01 13:24:31 +0300

    add FileEncodingReaderImpl and FileEncodingWriterImpl

---
+++ src/main/java/impl/file/FileEncodingReaderImpl.java
@@ -0,0 +1,20 @@
+package impl.file;
+
+import api.file.FileEncodingReader;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
+public class FileEncodingReaderImpl implements FileEncodingReader {
+    @Override
+    public Reader read(File file, Charset fileEncoding) {
+        try {
+            return new FileReader(file, fileEncoding);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to open file for reading", e);
+        }
+    }
+}
