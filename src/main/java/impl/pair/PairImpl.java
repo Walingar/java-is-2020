4f3c44f554404a60181f3426a9723c8ddfad6674
Alexey <alexeyyushchuk@rambler.com>
2020-06-01 18:58:05 +0300

    Pair Implementation

---
+++ src/main/java/impl/pair/PairImpl.java
@@ -0,0 +1,39 @@
+package impl.pair;
+
+import api.pair.Pair;
+
+import java.util.Objects;
+
+public class PairImpl<T, K> implements Pair<T, K> {
+    private T first;
+    private K second;
+
+    public PairImpl(T first, K second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public T getFirst() {
+        return first;
+    }
+
+    @Override
+    public K getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PairImpl<?, ?> pair = (PairImpl<?, ?>) o;
+        return Objects.equals(first, pair.first) &&
+                Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+}
