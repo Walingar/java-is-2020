0b3d3d6251b658605f932eaf1c1526b1bd65d20d
Sergey Stepanov <stepanov.s@numdes.com>
2020-06-01 13:24:31 +0300

    add FileEncodingReaderImpl and FileEncodingWriterImpl

---
+++ src/main/java/impl/file/FileEncodingWriterImpl.java
@@ -0,0 +1,33 @@
+package impl.file;
+
+import api.file.FileEncodingWriter;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class FileEncodingWriterImpl implements FileEncodingWriter {
+    @Override
+    public void write(File file, InputStream data, Charset dataEncoding) {
+        write(file, data, dataEncoding, StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public void write(File file, InputStream data, Charset dataEncoding, Charset fileEncoding) {
+        try {
+            File directory = new File((file.getParent()));
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            try (var dataReader = new InputStreamReader(data, dataEncoding);
+                 var fileWriter = new FileWriter(file.getAbsoluteFile(), fileEncoding)) {
+                int symbol;
+                while ((symbol = dataReader.read()) >= 0) {
+                    fileWriter.write(symbol);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to write to file", e);
+        }
+    }
+}
