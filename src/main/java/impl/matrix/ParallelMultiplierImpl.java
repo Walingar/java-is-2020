2a7ab1cc6b34ac89f6fa6b44be23faa4e3b55d39
Sergey Stepanov <stepanov.s@numdes.com>
2020-06-02 11:46:13 +0300

    implement concurrent way of matrix multiplication

--- src/main/java/impl/matrix/ParallelMultiplierImpl.java
+++ src/main/java/impl/matrix/ParallelMultiplierImpl.java
@@ -2,9 +2,14 @@ package impl.matrix;
 
 import api.matrix.ParallelMultiplier;
 
+import java.util.ArrayList;
+
 public class ParallelMultiplierImpl implements ParallelMultiplier {
 
     private final int maxThreadsCount;
+    private double[][] a;
+    private double[][] b;
+    private double[][] result;
 
     public ParallelMultiplierImpl(int maxThreadsCount) {
         this.maxThreadsCount = maxThreadsCount;
@@ -13,22 +18,63 @@ public class ParallelMultiplierImpl implements ParallelMultiplier {
     @Override
     public double[][] mul(double[][] a, double[][] b) {
         var aNumRows = a.length;
-        var aNumColumns = a[0].length;
-        var bNumRows = b.length;
         var bNumColumns = b[0].length;
-
 
         double[][] result = new double[aNumRows][bNumColumns];
 
-        for (var i = 0; i < aNumRows; i++) {
-            for (var j = 0; j < bNumColumns; j++) {
-                double dot = 0.0;
-                for (var k = 0; k < aNumColumns; k++) {
-                    dot += a[i][k] * b[k][j];
-                }
-                result[i][j] = dot;
+        var maxNumThreads = Math.min(aNumRows, maxThreadsCount);
+
+        ArrayList<Thread> threads = new ArrayList<>();
+        var startRowIndex = 0;
+        // https://stackoverflow.com/a/21830188
+        var numRowsDelta = (aNumRows + maxNumThreads - 1) / maxNumThreads;
+
+        for (var i = 0; i < maxNumThreads; i++) {
+            var endRowIndex = startRowIndex + numRowsDelta - 1;
+            var thread = new Thread(new ConcurrentWorker(a, b, result, startRowIndex, endRowIndex));
+            threads.add(thread);
+            thread.start();
+            startRowIndex = endRowIndex + 1;
+        }
+
+        for (var thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("Catched error in thread.");
             }
         }
         return result;
     }
+
+    private class ConcurrentWorker implements Runnable {
+        private double[][] a;
+        private double[][] b;
+        private double[][] result;
+        private int startRowIndex;
+        private int endRowIndex;
+
+        public ConcurrentWorker(double[][] a, double[][] b, double[][] result, int startRowIndex, int endRowIndex) {
+            this.a = a;
+            this.b = b;
+            this.result = result;
+            this.startRowIndex = startRowIndex;
+            this.endRowIndex = endRowIndex;
+        }
+
+        @Override
+        public void run() {
+            var aNumColumns = a[0].length;
+            var bNumColumns = b[0].length;
+
+            for (int i = startRowIndex; i <= endRowIndex; i++) {
+                for (var j = 0; j < bNumColumns; j++) {
+                    for (var k = 0; k < aNumColumns; k++) {
+                        result[i][j] += a[i][k] * b[k][j];
+                    }
+                }
+            }
+        }
+    }
+
 }
