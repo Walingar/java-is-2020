55905298105ae9f7a043114ea98e19d531a7a19c
Andrei Vorontsov <andrei.vorontsov@nordigy.ru>
2020-06-02 17:56:18 +0300

    task 6

---
+++ src/main/java/impl/pair/NumberPairImpl.java
@@ -0,0 +1,9 @@
+package impl.pair;
+
+import api.pair.NumberPair;
+
+public class NumberPairImpl<T extends Number,K extends Number> extends PairImpl<T,K> implements NumberPair<T,K> {
+    public NumberPairImpl(T _1, K _2) {
+        super(_1, _2);
+    }
+}
