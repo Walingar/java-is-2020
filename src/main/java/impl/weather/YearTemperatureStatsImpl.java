7ee580424c7a2ab307e5e63ab6a3113f42d9376e
Andy1897 <34788382+Andy1897@users.noreply.github.com>
2020-06-08 00:05:00 +0300

    add changes

--- src/main/java/impl/weather/YearTemperatureStatsImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsImpl.java
@@ -2,71 +2,100 @@ package impl.weather;
 
 import api.weather.DayTemperatureInfo;
 import api.weather.YearTemperatureStats;
-
 import java.time.Month;
 import java.util.*;
 
-import static java.util.Comparator.comparing;
+import static java.lang.Math.max;
 
 public class YearTemperatureStatsImpl implements YearTemperatureStats {
 
-    private Map<Month, Integer> maxTemperature = new HashMap<>();
-    private Map<Month, Double> avgTemperature = new HashMap<>();
-    private Map<Month, Map<Integer, DayTemperatureInfo>> yearStats = new HashMap<>();
+    private Map<Month, MonthInfo> monthInfo;
+
+    YearTemperatureStatsImpl() {
+        monthInfo = new HashMap<>();
+    }
 
     @Override
     public void updateStats(DayTemperatureInfo info) {
         Month month = info.getMonth();
-        int day = info.getDay();
-        int temperature = info.getTemperature();
-
-        if (!yearStats.containsKey(month)) {
-            maxTemperature.put(month, temperature);
-            avgTemperature.put(month, (double) temperature);
-            yearStats.put(month, new LinkedHashMap<>());
-        }
-
-        yearStats.get(month).put(day, info);
-        int dayCounter = yearStats.get(month).size();
-
-        if (dayCounter != 1) {
-            if (maxTemperature.get(month) < temperature) {
-                maxTemperature.put(month, temperature);
-            }
-
-            double newAverageTemperature = (avgTemperature.get(month) * (dayCounter - 1) + temperature) / dayCounter;
-
-            avgTemperature.put(month, newAverageTemperature);
-        }
-    }
-
-    @Override
-    public Double getAverageTemperature(Month month) {
-        return avgTemperature.get(month);
-    }
-
-    @Override
-    public Map<Month, Integer> getMaxTemperature() {
-        return maxTemperature;
-    }
-
-    @Override
-    public List<DayTemperatureInfo> getSortedTemperature(Month month) {
-        if (yearStats.getOrDefault(month, null) == null) {
-            return new ArrayList<>();
-        }
-        Map<Integer, DayTemperatureInfo> temp = yearStats.getOrDefault(month, null);
-        ArrayList<DayTemperatureInfo> List = new ArrayList<>(temp.values());
-        List.sort(comparing(DayTemperatureInfo::getTemperature));
-        return List;
+        monthInfo.putIfAbsent(month, new MonthInfo());
+        monthInfo.get(month).update(info);
     }
 
     @Override
     public DayTemperatureInfo getTemperature(int day, Month month) {
-        var stat = yearStats.get(month);
-        if (stat != null) {
-            return stat.get(day);
+        MonthInfo info = monthInfo.get(month);
+        if (info == null) {
+            return null;
         }
-        return null;
+        return info.getTemperature(day);
+    }
+
+    @Override
+    public Double getAverageTemperature(Month month) {
+        MonthInfo info = monthInfo.get(month);
+        if (info == null) {
+            return null;
+        }
+        return info.getAverageTemperature();
+    }
+
+    @Override
+    public Map<Month, Integer> getMaxTemperature() {
+        Map<Month, Integer> maxTemperatures = new HashMap<>();
+        monthInfo.forEach((k, v) -> maxTemperatures.put(k, v.getMaximalTemperature()));
+        return maxTemperatures;
+    }
+
+    @Override
+    public List<DayTemperatureInfo> getSortedTemperature(Month month) {
+        MonthInfo info = monthInfo.get(month);
+        if (info == null) {
+            return new ArrayList<>();
+        }
+        return info.getSortedTemperature();
+    }
+
+    private static class MonthInfo {
+        private final Map<Integer, DayTemperatureInfo> temperatures;
+        private Integer maximalTemperature;
+        private Double sumTemperature;
+
+        MonthInfo() {
+            maximalTemperature = null;
+            sumTemperature = 0.0;
+            temperatures = new LinkedHashMap<>();
+        }
+
+        double getAverageTemperature() {
+            return sumTemperature / temperatures.size();
+        }
+
+        int getMaximalTemperature() {
+            return maximalTemperature;
+        }
+
+        void update(DayTemperatureInfo info) {
+            int day = info.getDay();
+            int temperature = info.getTemperature();
+            if (maximalTemperature == null) {
+                maximalTemperature = temperature;
+            } else {
+                maximalTemperature = max(temperature, maximalTemperature);
+            }
+            sumTemperature += temperature;
+            temperatures.put(day, info);
+        }
+
+        DayTemperatureInfo getTemperature(int day) {
+            return temperatures.get(day);
+        }
+
+        List<DayTemperatureInfo> getSortedTemperature() {
+            List<DayTemperatureInfo> sorted = new ArrayList<>(temperatures.size());
+            temperatures.forEach((k, v) -> sorted.add(v));
+            sorted.sort(Comparator.comparingInt(DayTemperatureInfo::getTemperature));
+            return sorted;
+        }
     }
 }
