0dd8f69cca54e87ee264e483561885ee79abe972
rznov <rznnova@gmail.com>
2020-06-11 16:06:44 +1100

    Refactor MonthTemperatureInfo

--- src/main/java/impl/weather/MonthTemperatureInfo.java
+++ src/main/java/impl/weather/MonthTemperatureInfo.java
@@ -4,34 +4,50 @@ import api.weather.DayTemperatureInfo;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import static java.util.Comparator.comparingDouble;
 
 public class MonthTemperatureInfo {
 
-   private LinkedHashMap<Integer, DayTemperatureInfo> dayInfos = new LinkedHashMap<>();
+    private final Map<Integer, DayTemperatureInfo> dayInfos = new LinkedHashMap<>();
+    private Integer max = -273;
+    private Double average = 0.0;
 
-   public void updateMonthStats(DayTemperatureInfo dayInfo) {
-       dayInfos.putIfAbsent(dayInfo.getDay(), dayInfo);
-   }
+    public void updateMonthStats(DayTemperatureInfo dayInfo) {
+        dayInfos.putIfAbsent(dayInfo.getDay(), dayInfo);
+        updateAverage(dayInfo.getTemperature(), dayInfos.size());
+        updateMax(dayInfo.getTemperature());
+    }
 
-   public double getAverageTemperature() {
-       return dayInfos.values().stream().mapToDouble(x-> x.getTemperature()).sum() / dayInfos.size();
+    public double getAverageTemperature() {
+        return average;
     }
 
     public Integer getMaxTemperature() {
-       if (!dayInfos.isEmpty())
-       {
-           return (int)dayInfos.values().stream().mapToDouble(x-> x.getTemperature()).max().getAsDouble();
-       }
-       return 0;
+        return max;
     }
 
     public List<DayTemperatureInfo> getSortedTemperature() {
-       return dayInfos.values().stream().sorted(comparingDouble(DayTemperatureInfo::getTemperature)).collect(Collectors.toList());
+        return dayInfos.values().stream().sorted(comparingDouble(DayTemperatureInfo::getTemperature)).collect(Collectors.toList());
     }
 
     public DayTemperatureInfo getTemperature(int day) {
-       return dayInfos.get(day);
+        return dayInfos.get(day);
+    }
+
+    private void updateMax(int temperature) {
+        if (temperature > max) {
+            max = temperature;
+        }
+    }
+
+    private void updateAverage(int temperature, int days) {
+        if (average == 0.0) {
+            average = (double) temperature;
+        } else {
+            average = (average * (days - 1) + temperature) / days;
+        }
     }
 }
+
