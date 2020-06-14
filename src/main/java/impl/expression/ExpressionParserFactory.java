3081486bb4ba2861e875277ba88ed474577bed36
lennuz17 <lennuz17@gmail.com>
2020-06-14 17:22:12 +0300

    fix code style

--- src/main/java/impl/expression/ExpressionParserFactory.java
+++ src/main/java/impl/expression/ExpressionParserFactory.java
@@ -1,10 +1,9 @@
 package impl.expression;
 
 import api.expression.ExpressionParser;
-import api.expression.ParseException;
 
 public class ExpressionParserFactory {
-    public static ExpressionParser getInstance(){
+    public static ExpressionParser getInstance() {
         return new ExpressionParserImpl();
     }
 }
