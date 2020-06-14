10aa1500708168b25d0934f4be820eaecb262f71
lennuz17 <lennuz17@gmail.com>
2020-06-14 19:15:40 +0300

    delete needless imports and correcting method createFile()

--- src/main/java/impl/file/FileEncodingReaderFactory.java
+++ src/main/java/impl/file/FileEncodingReaderFactory.java
@@ -2,12 +2,6 @@ package impl.file;
 
 import api.file.FileEncodingReader;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.charset.Charset;
-
 public class FileEncodingReaderFactory {
     public static FileEncodingReader getInstance() {
         return new FileEncodingReaderImpl();
