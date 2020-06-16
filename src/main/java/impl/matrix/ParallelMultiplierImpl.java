e330315e5fd6a178f06503e80506dc2d000621c5
ITLeopard <forest_spbu@mail.ru>
2020-06-17 02:04:59 +0300

    Solved task 8

---
+++ src/main/java/impl/matrix/ParallelMultiplierImpl.java
@@ -0,0 +1,43 @@
+package impl.matrix;
+
+import api.matrix.ParallelMultiplier;
+
+public class ParallelMultiplierImpl implements ParallelMultiplier {
+    int maxThreadsCount;
+
+    ParallelMultiplierImpl(int maxThreadsCount) {
+        this.maxThreadsCount = maxThreadsCount;
+    }
+
+    @Override
+    public double[][] mul(double[][] a, double[][] b) {
+        Thread[] t = new Thread[maxThreadsCount];
+        double[][] c = new double[a.length][b[0].length];
+
+        int step = (int) Math.ceil((double) a.length / maxThreadsCount);
+        int startI = 0;
+        int finishI = startI + step;
+
+        if (step * maxThreadsCount > a.length) {
+            maxThreadsCount = a.length;
+        }
+
+        for (int i = 0; i < maxThreadsCount; i++) {
+            t[i] = new Thread(new RunnableImpl(a, b, c, startI, finishI));
+            t[i].start();
+
+            startI += step;
+            finishI = Math.min(finishI + startI, a.length);
+        }
+
+        try {
+            for (int i = 0; i < maxThreadsCount; i++) {
+                t[i].join();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Error!");
+        }
+
+        return c;
+    }
+}
