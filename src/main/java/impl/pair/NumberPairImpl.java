888038be34bd3b42e7e03a3bf646de0c3c19a0b0
ITLeopard <forest_spbu@mail.ru>
2020-06-17 14:04:50 +0300

    Solved tasks from homework 6

---
+++ src/main/java/impl/pair/NumberPairImpl.java
@@ -0,0 +1,7 @@
+package impl.pair;
+
+public class NumberPairImpl<K, T> extends PairImpl<K, T> {
+    NumberPairImpl(K first, T second) {
+        super(first, second);
+    }
+}
--- src/main/java/impl/pair/PairFactory.java
+++ src/main/java/impl/pair/PairFactory.java
@@ -4,6 +4,6 @@ import api.pair.Pair;
 
 public class PairFactory {
     public static <K, T> Pair<K, T> of(K first, T second) {
-        return null;
+        return new PairImpl<>(first, second);
     }
 }
