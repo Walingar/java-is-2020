ab762a46b99a0293e8a1a7098218f9387b4d51fb
ITLeopard <forest_spbu@mail.ru>
2020-06-17 17:55:58 +0300

    Fix mistakes

--- src/main/java/queue/impl/ArrayQueueImpl.java
+++ src/main/java/queue/impl/ArrayQueueImpl.java
@@ -3,77 +3,89 @@ package queue.impl;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.AbstractQueue;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class ArrayQueueImpl extends AbstractQueue<Integer> {
-    private int[] mas = new int[5];
-    private int size = 0;
+    private int[] arr = new int[5];
+    private int lastIndex = 0;
+    private int firstIndex = 0;
 
     @NotNull
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<>() {
-            int currentIndex = 0;
-
-            @Override
-            public boolean hasNext() {
-                return currentIndex + 1 < size;
-            }
-
-            @Override
-            public Integer next() {
-                if (hasNext()) {
-                    currentIndex++;
-                    return mas[currentIndex];
-                } else {
-                    return null;
-                }
-            }
-        };
+        return new MyIterator();
     }
 
     @Override
     public int size() {
-        return size;
+        return lastIndex - firstIndex;
     }
 
     @Override
     public boolean offer(Integer integer) {
-        if (size == mas.length) {
-            int[] newMas = new int[size * 2];
-            System.arraycopy(mas, 0, newMas, 0, mas.length);
-            mas = newMas;
+        if (lastIndex == arr.length) {
+            int[] newMas = new int[lastIndex * 2];
+            System.arraycopy(arr, 0, newMas, 0, arr.length);
+            arr = newMas;
         }
-        mas[size] = integer;
-        size++;
+        arr[lastIndex] = integer;
+        lastIndex++;
         return true;
     }
 
     @Override
     public Integer poll() {
-        if (size > 0) {
-            Integer polledElement = mas[0];
-            size--;
-            if (size < mas.length / 2) {
-                int[] newMas = new int[size + 1];
-                System.arraycopy(mas, 1, newMas, 0, size);
-                mas = newMas;
-            } else {
-                System.arraycopy(mas, 1, mas, 0, size);
+        if (lastIndex - firstIndex > 0) {
+            Integer polledElement = arr[firstIndex];
+            firstIndex++;
+            if (lastIndex < arr.length / 2) {
+                int[] newMas = new int[lastIndex + 1];
+                System.arraycopy(arr, 0, newMas, 0, lastIndex);
+                arr = newMas;
             }
+            clearTrash();
             return polledElement;
         } else {
-            System.out.println("Nothing to poll! There are no elements in the queue");
             return null;
         }
     }
 
     @Override
     public Integer peek() {
-        if (size == 0) {
+        if (lastIndex - firstIndex == 0) {
             return null;
         } else {
-            return mas[0];
+            return arr[firstIndex];
+        }
+    }
+
+    private void clearTrash() {
+        if (firstIndex != 0 && firstIndex == lastIndex - firstIndex) {
+            int[] newMas = new int[lastIndex - firstIndex];
+            System.arraycopy(arr, firstIndex, newMas, 0, lastIndex - firstIndex);
+            arr = newMas;
+            lastIndex -= firstIndex;
+            firstIndex = 0;
+        }
+    }
+
+    private class MyIterator implements Iterator<Integer> {
+        int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex + 1 < lastIndex;
+        }
+
+        @Override
+        public Integer next() {
+            if (hasNext()) {
+                currentIndex++;
+                return arr[currentIndex];
+            } else {
+                return null;
+            }
         }
     }
 }
