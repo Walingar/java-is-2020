f092c5da4174664c586c7c55480bcd1b79ad8235
ITLeopard <forest_spbu@mail.ru>
2020-06-18 18:51:17 +0300

    Fixed mistakes

--- src/main/java/info/kgeorgiy/java/advanced/student/StudentDB.java
+++ src/main/java/info/kgeorgiy/java/advanced/student/StudentDB.java
@@ -2,22 +2,24 @@ package info.kgeorgiy.java.advanced.student;
 
 import java.util.*;
 import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StudentDB implements StudentQuery {
     @Override
     public List<String> getFirstNames(List<Student> students) {
-        return students.stream().map(Student::getFirstName).collect(Collectors.toList());
+        return getInfo(students, Student::getFirstName);
     }
 
     @Override
     public List<String> getLastNames(List<Student> students) {
-        return students.stream().map(Student::getLastName).collect(Collectors.toList());
+        return getInfo(students, Student::getLastName);
     }
 
     @Override
     public List<String> getGroups(List<Student> students) {
-        return students.stream().map(Student::getGroup).collect(Collectors.toList());
+        return getInfo(students, Student::getGroup);
     }
 
     @Override
@@ -49,45 +51,27 @@ public class StudentDB implements StudentQuery {
 
     @Override
     public List<Student> sortStudentsById(Collection<Student> students) {
-        return students
-                .stream()
-                .sorted(Comparator.comparing(Student::getId))
-                .collect(Collectors.toList());
+        return getSortedList(students, Comparator.comparing(Student::getId));
     }
 
     @Override
     public List<Student> sortStudentsByName(Collection<Student> students) {
-        return students
-                .stream()
-                .sorted(Comparator.comparing(s -> s.getLastName() + " " + s.getFirstName() + " " + s.getId()))
-                .collect(Collectors.toList());
+        return getSortedList(students, Comparator.comparing(s -> s.getLastName() + " " + s.getFirstName() + " " + s.getId()));
     }
 
     @Override
     public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
-        return students
-                .stream()
-                .filter(s -> s.getFirstName().equals(name))
-                .sorted(Comparator.comparing(s -> s.getLastName() + " " + s.getFirstName() + " " + s.getId()))
-                .collect(Collectors.toList());
+        return getFilteredSortedList(students, s -> s.getFirstName().equals(name));
     }
 
     @Override
     public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
-        return students
-                .stream()
-                .filter(s -> s.getLastName().equals(name))
-                .sorted(Comparator.comparing(s -> s.getLastName() + " " + s.getFirstName() + " " + s.getId()))
-                .collect(Collectors.toList());
+        return getFilteredSortedList(students, s -> s.getLastName().equals(name));
     }
 
     @Override
     public List<Student> findStudentsByGroup(Collection<Student> students, String group) {
-        return students
-                .stream()
-                .filter(s -> s.getGroup().equals(group))
-                .sorted(Comparator.comparing(s -> s.getLastName() + " " + s.getFirstName() + " " + s.getId()))
-                .collect(Collectors.toList());
+        return getFilteredSortedList(students, s -> s.getGroup().equals(group));
     }
 
     @Override
@@ -97,5 +81,25 @@ public class StudentDB implements StudentQuery {
                 .filter(s -> s.getGroup().equals(group))
                 .sorted(Comparator.comparing(s -> s.getLastName() + " " + s.getFirstName() + " " + s.getId()))
                 .collect(Collectors.toMap(Student::getLastName, Student::getFirstName, BinaryOperator.minBy(Comparable::compareTo)));
+    }
+
+    private <T> List<T> getInfo(List<Student> students, Function<Student, T> info) {
+        return students.stream().map(info).collect(Collectors.toList());
+    }
+
+    private List<Student> getSortedList(Collection<Student> students, Comparator<Student> comparator) {
+        return students
+                .stream()
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
+    private List<Student> getFilteredSortedList(Collection<Student> students, Predicate<Student> predicate) {
+        return students
+                .stream()
+                .filter(predicate)
+                .sorted(Comparator.comparing(s -> s.getLastName() + " " + s.getFirstName() + " " + s.getId()))
+                .collect(Collectors.toList());
+
     }
 }
