969ca5e468d3eae00489b64d839b38e9b3752c8c
Kirill Smirnov <smirnowku@gmail.com>
2020-06-19 01:02:43 +0300

    Modify FollowersStats implementation

--- src/main/java/impl/network/FollowersStatsImpl.java
+++ src/main/java/impl/network/FollowersStatsImpl.java
@@ -4,103 +4,35 @@ import api.network.FollowersStats;
 import api.network.SocialNetwork;
 import api.network.UserInfo;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
 import java.util.function.Predicate;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.Collections.singleton;
 
 public class FollowersStatsImpl implements FollowersStats {
 
-    private final UsersCache usersCache;
-    private final int parallelism;
+    private final SocialNetwork network;
 
     public FollowersStatsImpl(SocialNetwork network) {
-        this(network, 2);
-    }
-
-    public FollowersStatsImpl(SocialNetwork network, int parallelism) {
-        this.usersCache = new UsersCache(network);
-        this.parallelism = parallelism;
+        this.network = network;
     }
 
     @Override
-    public Future<Integer> followersCountBy(int userId, int maxDepth, Predicate<UserInfo> filter) {
-        var threadPool = new ForkJoinPool(parallelism);
-        var futureResult = threadPool.submit(new Task(userId, maxDepth, filter));
-        threadPool.shutdown();
-        return futureResult;
-    }
-
-    private static class UsersCache {
-
-        private final SocialNetwork socialNetwork;
-        private final Map<Integer, UserInfo> info = Collections.synchronizedMap(new HashMap<>());
-        private final Map<Integer, Collection<Integer>> followersIds = Collections.synchronizedMap(new HashMap<>());
-
-        public UsersCache(SocialNetwork socialNetwork) {
-            this.socialNetwork = socialNetwork;
-        }
-
-        public UserInfo getInfo(int userId) {
-            return info.computeIfAbsent(userId, id -> socialNetwork.getUserInfo(id).join());
-        }
-
-        public Collection<Integer> getFollowersIds(int userId) {
-            return followersIds.computeIfAbsent(userId, id -> socialNetwork.getFollowers(id).join());
-        }
-    }
-
-    private class Task extends RecursiveTask<Integer> {
-
-        private final int userId;
-        private final int depth;
-        private final int maxDepth;
-        private final Predicate<UserInfo> filter;
-        private final Map<Integer, Object> visitedNodes;
-
-        public Task(int userId, int maxDepth, Predicate<UserInfo> filter) {
-            this.userId = userId;
-            this.depth = 0;
-            this.maxDepth = maxDepth;
-            this.filter = filter;
-            this.visitedNodes = Collections.synchronizedMap(new HashMap<>());
-        }
-
-        public Task(int userId, int depth, int maxDepth, Predicate<UserInfo> filter, Map<Integer, Object> visitedNodes) {
-            this.userId = userId;
-            this.depth = depth;
-            this.maxDepth = maxDepth;
-            this.filter = filter;
-            this.visitedNodes = visitedNodes;
-        }
-
-        @Override
-        protected Integer compute() {
-            if (depth > maxDepth || visitedNodes.containsKey(userId)) {
-                return 0;
+    public Future<Integer> followersCountBy(int id, int depth, Predicate<UserInfo> predicate) {
+        return CompletableFuture.supplyAsync(() -> {
+            CompletableFuture<Integer> total = CompletableFuture.completedFuture(0);
+            Search initialSearch = new Search(id, depth, predicate, network);
+            Queue<Search> pendingSearches = new LinkedList<>(singleton(initialSearch));
+            while (!pendingSearches.isEmpty()) {
+                Search search = pendingSearches.remove();
+                Search.Result result = search.execute();
+                pendingSearches.addAll(result.subSearches.join());
+                total = result.isFilterSatisfied.thenCombine(total, (satisfied, count) -> satisfied ? count + 1 : count);
             }
-            visitedNodes.put(userId, false);
-            var filterSatisfied = CompletableFuture.supplyAsync(this::isFilterSatisfied);
-            var subTasks = CompletableFuture.supplyAsync(this::prepareSubTasks);
-            CompletableFuture.allOf(filterSatisfied, subTasks).join();
-            return (filterSatisfied.join() ? 1 : 0) + ForkJoinTask.invokeAll(subTasks.join()).stream()
-                    .mapToInt(ForkJoinTask::join)
-                    .sum();
-        }
-
-        private boolean isFilterSatisfied() {
-            var user = usersCache.getInfo(userId);
-            return filter.test(user);
-        }
-
-        private Collection<Task> prepareSubTasks() {
-            return usersCache.getFollowersIds(userId).stream()
-                    .map(id -> new Task(id, depth + 1, maxDepth, filter, visitedNodes))
-                    .collect(toList());
-        }
+            return total.join();
+        });
     }
 }
---
+++ src/main/java/impl/network/Search.java
@@ -0,0 +1,65 @@
+package impl.network;
+
+import api.network.SocialNetwork;
+import api.network.UserInfo;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Predicate;
+
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toList;
+
+public class Search {
+
+    private final int userId;
+    private final int depth;
+    private final int maxDepth;
+    private final Predicate<UserInfo> filter;
+    private final SocialNetwork network;
+    private final Set<Integer> visitedNodes;
+
+    public Search(int userId, int maxDepth, Predicate<UserInfo> filter, SocialNetwork network) {
+        this(userId, 0, maxDepth, filter, network, new HashSet<>());
+    }
+
+    private Search(int userId, int depth, int maxDepth, Predicate<UserInfo> filter,
+                   SocialNetwork network, Set<Integer> visitedNodes) {
+        this.userId = userId;
+        this.depth = depth;
+        this.maxDepth = maxDepth;
+        this.filter = filter;
+        this.network = network;
+        this.visitedNodes = visitedNodes;
+    }
+
+    public Result execute() {
+        if (depth > maxDepth || visitedNodes.contains(userId)) {
+            return Result.empty();
+        }
+        visitedNodes.add(userId);
+        CompletableFuture<Boolean> isFilterSatisfied = network.getUserInfo(userId).thenApply(filter::test);
+        CompletableFuture<Collection<Search>> subSearches = network.getFollowers(userId).thenApply(ids -> ids.stream()
+                .map(id -> new Search(id, depth + 1, maxDepth, filter, network, visitedNodes))
+                .collect(toList())
+        );
+        return new Result(isFilterSatisfied, subSearches);
+    }
+
+    public static class Result {
+
+        public final CompletableFuture<Boolean> isFilterSatisfied;
+        public final CompletableFuture<Collection<Search>> subSearches;
+
+        public Result(CompletableFuture<Boolean> isFilterSatisfied, CompletableFuture<Collection<Search>> subSearches) {
+            this.isFilterSatisfied = isFilterSatisfied;
+            this.subSearches = subSearches;
+        }
+
+        public static Result empty() {
+            return new Result(CompletableFuture.completedFuture(false), CompletableFuture.completedFuture(emptyList()));
+        }
+    }
+}
