22545344078b615a56ac21b1f48dae3ee3c4abf6
Ammar Ali <35392687+ammarali32@users.noreply.github.com>
2020-06-18 17:47:45 +0300

    v7

--- src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
@@ -10,9 +10,9 @@ import java.util.Collection;
 public class YearTemperatureStatsParserImpl implements YearTemperatureStatsParser {
 
     @Override
-    public YearTemperatureStats parse(Collection<String> rawData) {
+    public YearTemperatureStats parse(Collection < String > rawData) {
         YearTemperatureStats status = new YearTemperatureStatsImpl();
-        for (String input : rawData) {
+        for (String input: rawData) {
             var split = input.split("[. ]");
             var day = Integer.parseInt(split[0]);
             var month = Month.of(Integer.parseInt(split[1]));
