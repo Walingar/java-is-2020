05fb9c01359c647ad46b97651cea74cc90d1b46c
Alexey <alexeyyushchuk@rambler.com>
2020-06-19 19:04:19 +0300

    Fixes

--- src/main/java/queue/impl/LinkedQueueImpl.java
+++ src/main/java/queue/impl/LinkedQueueImpl.java
@@ -82,4 +82,35 @@ public class LinkedQueueImpl extends AbstractQueue<Integer> {
 
         return firstElement.getValue();
     }
+
+    private class QueueNodeImpl {
+        private Integer value;
+
+        private QueueNodeImpl next;
+        private QueueNodeImpl previous;
+
+        public QueueNodeImpl(Integer value) {
+            this.value = value;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public QueueNodeImpl getNext() {
+            return next;
+        }
+
+        public QueueNodeImpl getPrevious() {
+            return previous;
+        }
+
+        public void setNext(QueueNodeImpl next) {
+            this.next = next;
+        }
+
+        public void setPrevious(QueueNodeImpl previous) {
+            this.previous = previous;
+        }
+    }
 }
