19e9484591ac095ccbb03b7a2a154acd1f5d9d9e
Alexey <alexeyyushchuk@rambler.com>
2020-06-19 19:26:48 +0300

    Fixes

--- src/main/java/impl/pair/NumberPairFactory.java
+++ src/main/java/impl/pair/NumberPairFactory.java
@@ -4,6 +4,6 @@ import api.pair.Pair;
 
 public class NumberPairFactory {
     public static <K extends Number, T extends Number> Pair<K, T> of(K first, T second) {
-        return PairFactory.of(first, second);
+        return new NumberPairImpl(first, second);
     }
 }
--- src/main/java/impl/pair/PairImpl.java
+++ src/main/java/impl/pair/PairImpl.java
@@ -25,11 +25,14 @@ public class PairImpl<T, K> implements Pair<T, K> {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         PairImpl<?, ?> pair = (PairImpl<?, ?>) o;
-        return Objects.equals(first, pair.first) &&
-                Objects.equals(second, pair.second);
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
     }
 
     @Override
