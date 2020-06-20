8f310a3253bf7ee733ab70bed71bebc60443cdc7
be39na <61196684+be39na@users.noreply.github.com>
2020-06-20 17:41:16 +0300

    code style fixe's

--- src/main/java/impl/expression/ExpressionParserImplementation.java
+++ src/main/java/impl/expression/ExpressionParserImplementation.java
@@ -7,26 +7,21 @@ public class ExpressionParserImplementation implements ExpressionParser {
     public int parse(String expression) throws ParseException {
         int result = 0;
         StringBuilder bufString = new StringBuilder();
-
         if (expression == null) {
             throw new IllegalArgumentException("NULL expression");
         }
-
 
         if (expression.isBlank()) {
             throw new IllegalArgumentException("Blank expression");
         }
 
-        if(!expression.contains("+")&&!expression.contains("-") )
-        {
+        if (!expression.contains("+") && !expression.contains("-")) {
             try {
                 result = Math.addExact(result, Integer.parseInt(expression.toString()));
-                return  result;
-            }
-            catch (NumberFormatException a) {
+                return result;
+            } catch (NumberFormatException a) {
                 throw new ParseException("Incorrect format");
             }
-
         }
         for (int i = 0; i < expression.length(); i++) {
             char bufSymbol = expression.charAt(i);
@@ -35,10 +30,7 @@ public class ExpressionParserImplementation implements ExpressionParser {
                 bufString.append(bufSymbol);
             }
 
-
-
-           //"1 + 12 + 3 + 564 + 123"
-            if (bufSymbol == '+' || bufSymbol == '-'||i==(expression.length()-1)) {
+            if (bufSymbol == '+' || bufSymbol == '-' || i == (expression.length() - 1)) {
                 if (bufString.length() != 0) {
                     try {
                         result = Math.addExact(result, Integer.parseInt(bufString.toString()));
@@ -49,7 +41,6 @@ public class ExpressionParserImplementation implements ExpressionParser {
                 }
                 bufString.append(bufSymbol);
             }
-
         }
         return result;
     }
