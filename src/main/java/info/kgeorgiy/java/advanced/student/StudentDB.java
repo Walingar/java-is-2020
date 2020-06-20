b02fe3ff989faf40410153919889973d088f8a7f
Andy1897 <34788382+Andy1897@users.noreply.github.com>
2020-06-20 17:36:44 +0300

    add changes

--- src/main/java/info/kgeorgiy/java/advanced/student/StudentDB.java
+++ src/main/java/info/kgeorgiy/java/advanced/student/StudentDB.java
@@ -1,6 +1,7 @@
 package info.kgeorgiy.java.advanced.student;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -8,35 +9,27 @@ public class StudentDB implements StudentQuery {
 
     @Override
     public List<String> getFirstNames(List<Student> students) {
-        return students.stream()
-                .map(student -> student.getFirstName())
-                .collect(Collectors.toList());
+        return getMappedList(students, Student::getFirstName);
     }
 
     @Override
     public List<String> getLastNames(List<Student> students) {
-        return students.stream()
-                .map(student -> student.getLastName())
-                .collect(Collectors.toList());
+        return getMappedList(students, Student::getLastName);
     }
 
     @Override
     public List<String> getGroups(List<Student> students) {
-        return students.stream()
-                .map(student -> student.getGroup())
-                .collect(Collectors.toList());
+        return getMappedList(students, Student::getGroup);
     }
 
     @Override
     public List<String> getFullNames(List<Student> students) {
-        return students.stream()
-                .map(student -> student.getFirstName() + " " + student.getLastName())
-                .collect(Collectors.toList());
+        return getMappedList(students, s -> s.getFirstName() + " " + s.getLastName());
     }
 
     @Override
     public Set<String> getDistinctFirstNames(List<Student> students) {
-        return new HashSet<>(getFirstNames(students));
+        return new HashSet<>(getMappedList(students, Student::getFirstName));
     }
 
     @Override
@@ -49,38 +42,44 @@ public class StudentDB implements StudentQuery {
 
     @Override
     public List<Student> sortStudentsById(Collection<Student> students) {
-        return (List<Student>) getSortedCollection(students, Comparator.comparingInt(Student::getId));
+        return getSortedList(students, Comparator.comparingInt(Student::getId));
     }
 
     @Override
     public List<Student> sortStudentsByName(Collection<Student> students) {
-        return (List<Student>) getSortedCollection(students, Comparator.comparing(Student::getLastName)
+        return getSortedList(students, Comparator.comparing(Student::getLastName)
                 .thenComparing(Student::getFirstName)
                 .thenComparing(Student::getId));
     }
 
     @Override
     public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
-        return sortStudentsByName(getFilteredCollection(students, s -> s.getFirstName().equals(name)));
+        return sortStudentsByName(getFilteredList(students, s -> s.getFirstName().equals(name)));
     }
 
     @Override
     public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
-        return sortStudentsByName(getFilteredCollection(students, s -> s.getLastName().equals(name)));
+        return sortStudentsByName(getFilteredList(students, s -> s.getLastName().equals(name)));
     }
 
     @Override
     public List<Student> findStudentsByGroup(Collection<Student> students, String group) {
-        return sortStudentsByName(getFilteredCollection(students, s -> s.getGroup().equals(group)));
+        return sortStudentsByName(getFilteredList(students, s -> s.getGroup().equals(group)));
     }
 
-    private <T> Collection<T> getFilteredCollection(Collection<T> collection, Predicate<T> predicate) {
+    private <T, R> List<R> getMappedList(Collection<T> collection, Function<T, R> function) {
+        return collection.stream()
+                .map(function)
+                .collect(Collectors.toList());
+    }
+
+    private <T> List<T> getFilteredList(Collection<T> collection, Predicate<T> predicate) {
         return collection.stream()
                 .filter(predicate)
                 .collect(Collectors.toList());
     }
 
-    private <T> Collection<T> getSortedCollection(Collection<T> collection, Comparator<T> comparator) {
+    private <T> List<T> getSortedList(Collection<T> collection, Comparator<T> comparator) {
         return collection.stream()
                 .sorted(comparator)
                 .collect(Collectors.toList());
@@ -88,8 +87,7 @@ public class StudentDB implements StudentQuery {
 
     @Override
     public Map<String, String> findStudentNamesByGroup(Collection<Student> students, String group) {
-        return students.stream()
-                .filter(student -> student.getGroup() == group)
+        return findStudentsByGroup(students, group).stream()
                 .collect(Collectors.toMap(Student::getLastName, Student::getFirstName, (a, b) -> a.compareTo(b) < 0 ? a : b));
     }
 }
