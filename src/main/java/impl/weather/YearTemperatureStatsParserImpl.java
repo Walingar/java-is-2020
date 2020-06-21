0e597004390a1ec1f5516baea33aa58cbca6b108
be39na <61196684+be39na@users.noreply.github.com>
2020-06-21 19:07:48 +0300

    Review and code style fixes

--- src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsParserImpl.java
@@ -9,7 +9,7 @@ import java.util.Collection;
 public class YearTemperatureStatsParserImpl implements YearTemperatureStatsParser {
     @Override
     public YearTemperatureStats parse(Collection<String> rawData) {
-        YearTemperatureStats result =  YearTemperatureStatsFactory.getInstance();
+        YearTemperatureStats result = YearTemperatureStatsFactory.getInstance();
         for (String str : rawData) {
             var rawStr = str.split("[. ]");
             result.updateStats(new DayTemperatureInfoImpl(Integer.parseInt(rawStr[0]), Month.of(Integer.parseInt(rawStr[1])), Integer.parseInt((rawStr[2]))));
