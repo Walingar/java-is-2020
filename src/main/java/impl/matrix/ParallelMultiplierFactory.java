21341160aad2d0dc477cc26a4788620d198e8e44
Dmitry <DiamindFaceit@yandex.ru>
2020-06-21 23:30:33 +0300

    [HW_8] add ParallelMultiplier implementation

--- src/main/java/impl/matrix/ParallelMultiplierFactory.java
+++ src/main/java/impl/matrix/ParallelMultiplierFactory.java
@@ -1,9 +1,10 @@
 package impl.matrix;
 
 import api.matrix.ParallelMultiplier;
+import api.matrix.ParallelMultiplierImpl;
 
 public class ParallelMultiplierFactory {
     public static ParallelMultiplier getInstance(int maxThreadsCount) {
-        return null;
+        return new ParallelMultiplierImpl(maxThreadsCount);
     }
 }
