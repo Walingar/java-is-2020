689fd70ee7a3aafd0e0dd84dea44a5e656076409
semenckin.e <semenckin.e@yandex.ru>
2020-06-22 23:57:44 +0300

    Code style

--- src/main/java/impl/matrix/Worker.java
+++ src/main/java/impl/matrix/Worker.java
@@ -8,7 +8,7 @@ public class Worker implements Runnable {
     private final int tasksCount;
     private final int maxThreadsCount;
 
-    Worker(double[][] a,double[][] b, double[][] result,int workerIndex,int maxThreadsCount){
+    Worker(double[][] a, double[][] b, double[][] result, int workerIndex, int maxThreadsCount) {
         this.a = a;
         this.b = b;
         this.result = result;
@@ -16,22 +16,22 @@ public class Worker implements Runnable {
         this.maxThreadsCount = maxThreadsCount;
         var totalTaskCount = a.length * b[0].length;
         this.tasksCount = workerIndex < totalTaskCount % maxThreadsCount ?
-                totalTaskCount / maxThreadsCount + 1 : totalTaskCount/maxThreadsCount;
+                totalTaskCount / maxThreadsCount + 1 : totalTaskCount / maxThreadsCount;
     }
 
     @Override
     public void run() {
-        for(var i = 0;i < tasksCount; i++){
+        for (var i = 0; i < tasksCount; i++) {
             var tackNum = i * maxThreadsCount + workerIndex;
             var row = tackNum / b[0].length;
             var column = tackNum % b[0].length;
-            result[row][column] = calculate(row,column);
+            result[row][column] = calculate(row, column);
         }
     }
 
-    private double calculate(int row,int column){
+    private double calculate(int row, int column) {
         double result = 0;
-        for(var i = 0;i < b.length; i++){
+        for (var i = 0; i < b.length; i++) {
             result += a[row][i] * b[i][column];
         }
 
