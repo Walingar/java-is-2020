183d32a61674b108e3ff032f52d22fbf47bf4afd
semenckin.e <semenckin.e@yandex.ru>
2020-06-25 01:53:31 +0300

    Review changes

--- src/main/java/impl/weather/DayTemperature.java
+++ src/main/java/impl/weather/DayTemperature.java
@@ -1,14 +1,13 @@
 package impl.weather;
 
 import api.weather.DayTemperatureInfo;
-
 import java.time.Month;
 
 public class DayTemperature implements DayTemperatureInfo {
 
-    private int day;
+    private final int day;
     private final Month month;
-    private int temperature;
+    private final int temperature;
 
     public DayTemperature(int day,int month,int temperature){
         this.day = day;
--- src/main/java/impl/weather/YearTemperatureStatsImpl.java
+++ src/main/java/impl/weather/YearTemperatureStatsImpl.java
@@ -7,41 +7,41 @@ import java.time.Month;
 import java.util.*;
 
 public class YearTemperatureStatsImpl implements YearTemperatureStats {
-    private final Map<Month,MonthTemperatureStats> yearMonthMap = new LinkedHashMap<Month,MonthTemperatureStats>();
+    private final Map<Month, MonthTemperatureStats> yearMonthMap = new HashMap<Month, MonthTemperatureStats>();
 
     @Override
     public void updateStats(DayTemperatureInfo info) {
         Objects.requireNonNull(info, "Info is null");
         var month = info.getMonth();
-        yearMonthMap.computeIfAbsent(month,k-> new MonthTemperatureStats()).updateStats(info);
+        yearMonthMap.computeIfAbsent(month, k -> new MonthTemperatureStats()).updateStats(info);
     }
 
     @Override
     public Double getAverageTemperature(Month month) {
-        Objects.requireNonNull(month,"Month could not be null");
+        Objects.requireNonNull(month, "Month could not be null");
         var monthInfo = yearMonthMap.get(month);
         return (monthInfo == null) ? null : monthInfo.getAvgTemperature();
     }
 
     @Override
     public Map<Month, Integer> getMaxTemperature() {
-        var result = new HashMap<Month,Integer>();
-        for(Map.Entry<Month,MonthTemperatureStats> entry : yearMonthMap.entrySet()){
-            result.put(entry.getKey(),entry.getValue().getMaxTemperature());
+        var result = new HashMap<Month, Integer>();
+        for (Map.Entry<Month, MonthTemperatureStats> entry : yearMonthMap.entrySet()) {
+            result.put(entry.getKey(), entry.getValue().getMaxTemperature());
         }
         return result;
     }
 
     @Override
     public List<DayTemperatureInfo> getSortedTemperature(Month month) {
-        Objects.requireNonNull(month,"Month could not be null");
+        Objects.requireNonNull(month, "Month could not be null");
         var monthInfo = yearMonthMap.get(month);
         return (monthInfo == null) ? Collections.emptyList() : monthInfo.getSortedTemperature();
     }
 
     @Override
     public DayTemperatureInfo getTemperature(int day, Month month) {
-        Objects.requireNonNull(month,"Month could not be null");
+        Objects.requireNonNull(month, "Month could not be null");
         var monthInfo = yearMonthMap.get(month);
         return (monthInfo == null) ? null : monthInfo.getDayTemperatureInfo(day);
     }
