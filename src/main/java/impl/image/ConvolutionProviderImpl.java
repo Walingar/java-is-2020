924d793ae783c172900a0fb6ab6ca9620e551ddd
Danil <abraham_lincoln5@rambler.ru>
2020-06-28 02:54:25 +0300

    corrections

--- src/main/java/impl/image/ConvolutionProviderImpl.java
+++ src/main/java/impl/image/ConvolutionProviderImpl.java
@@ -12,18 +12,17 @@ public class ConvolutionProviderImpl implements ConvolutionProvider {
         Color[][] filteredImage = new Color[rowNum][colNum];
         for (int row = 0; row < rowNum; row++) {
             for (int column = 0; column < colNum; column++) {
-                filteredImage[row][column] = ConvolutionProviderImpl.applyToPixel(image,kernel,row,column);
-                System.out.println(column);
+                filteredImage[row][column] = ConvolutionProviderImpl.applyToPixel(image, kernel, row, column);
             }
         }
         return filteredImage;
     }
 
     private static Color applyToPixel(Color[][] image, double[][] kernel, int imageRow, int imageColumn) {
-        int red     = 0;
-        int green   = 0;
-        int blue    = 0;
-        int kernelRadius = kernel.length/2;
+        int red = 0;
+        int green = 0;
+        int blue = 0;
+        int kernelRadius = kernel.length / 2;
         for (int rowShift = -kernelRadius; rowShift <= kernelRadius; rowShift++) {
             for (int columnShift = -kernelRadius; columnShift <= kernelRadius; columnShift++) {
                 int rowIndex = imageRow + rowShift;
