d2b761963c3bde48c05af64859bbd23f0f431b0a
Sergey Stepanov <stepanov.s@numdes.com>
2020-06-27 12:01:02 +0300

    fix code for review

--- src/main/java/impl/matrix/ParallelMultiplierImpl.java
+++ src/main/java/impl/matrix/ParallelMultiplierImpl.java
@@ -3,13 +3,11 @@ package impl.matrix;
 import api.matrix.ParallelMultiplier;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ParallelMultiplierImpl implements ParallelMultiplier {
 
     private final int maxThreadsCount;
-    private double[][] a;
-    private double[][] b;
-    private double[][] result;
 
     public ParallelMultiplierImpl(int maxThreadsCount) {
         this.maxThreadsCount = maxThreadsCount;
@@ -22,19 +20,19 @@ public class ParallelMultiplierImpl implements ParallelMultiplier {
 
         double[][] result = new double[aNumRows][bNumColumns];
 
-        var maxNumThreads = Math.min(aNumRows, maxThreadsCount);
+        var maxNumThreads = Math.min(aNumRows * bNumColumns, maxThreadsCount);
 
-        ArrayList<Thread> threads = new ArrayList<>();
-        var startRowIndex = 0;
+        List<Thread> threads = new ArrayList<>();
+        var startFlatIndex = 0;
         // https://stackoverflow.com/a/21830188
-        var numRowsDelta = (aNumRows + maxNumThreads - 1) / maxNumThreads;
+        var flatIndexDelta = (aNumRows * bNumColumns + maxNumThreads - 1) / maxNumThreads;
 
         for (var i = 0; i < maxNumThreads; i++) {
-            var endRowIndex = startRowIndex + numRowsDelta - 1;
-            var thread = new Thread(new ConcurrentWorker(a, b, result, startRowIndex, endRowIndex));
+            var endFlatIndex = startFlatIndex + flatIndexDelta - 1;
+            var thread = new Thread(new ConcurrentWorker(a, b, result, startFlatIndex, endFlatIndex));
             threads.add(thread);
             thread.start();
-            startRowIndex = endRowIndex + 1;
+            startFlatIndex = endFlatIndex + 1;
         }
 
         for (var thread : threads) {
@@ -47,31 +45,34 @@ public class ParallelMultiplierImpl implements ParallelMultiplier {
         return result;
     }
 
-    private class ConcurrentWorker implements Runnable {
-        private double[][] a;
-        private double[][] b;
-        private double[][] result;
-        private int startRowIndex;
-        private int endRowIndex;
 
-        public ConcurrentWorker(double[][] a, double[][] b, double[][] result, int startRowIndex, int endRowIndex) {
+
+    private static class ConcurrentWorker implements Runnable {
+        private final double[][] a;
+        private final double[][] b;
+        private final double[][] result;
+        private final int startFlatIndex;
+        private final int endFlatIndex;
+
+        public ConcurrentWorker(double[][] a, double[][] b, double[][] result, int startFlatIndex, int endFlatIndex) {
             this.a = a;
             this.b = b;
             this.result = result;
-            this.startRowIndex = startRowIndex;
-            this.endRowIndex = endRowIndex;
+            this.startFlatIndex = startFlatIndex;
+            this.endFlatIndex = endFlatIndex;
         }
 
         @Override
         public void run() {
-            var aNumColumns = a[0].length;
+            var bNumRows = b.length;
             var bNumColumns = b[0].length;
 
-            for (int i = startRowIndex; i <= endRowIndex; i++) {
-                for (var j = 0; j < bNumColumns; j++) {
-                    for (var k = 0; k < aNumColumns; k++) {
-                        result[i][j] += a[i][k] * b[k][j];
-                    }
+            for (var i = startFlatIndex; i <= endFlatIndex; i++) {
+                var row = i / bNumColumns;
+                var column = i % bNumColumns;
+                result[row][column] = 0;
+                for (var k = 0; k < bNumRows; k++) {
+                    result[row][column] += a[row][k] * b[k][column];
                 }
             }
         }
