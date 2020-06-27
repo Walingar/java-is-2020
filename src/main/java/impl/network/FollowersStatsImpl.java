573802b23275ec335cac2caccea0b5112cba4021
Kirill Smirnov <smirnowku@gmail.com>
2020-06-27 16:39:35 +0300

    Improve FollowersStats: remove synchronous code

--- src/main/java/impl/network/FollowersStatsImpl.java
+++ src/main/java/impl/network/FollowersStatsImpl.java
@@ -4,13 +4,9 @@ import api.network.FollowersStats;
 import api.network.SocialNetwork;
 import api.network.UserInfo;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 import java.util.function.Predicate;
-
-import static java.util.Collections.singleton;
 
 public class FollowersStatsImpl implements FollowersStats {
 
@@ -22,17 +18,17 @@ public class FollowersStatsImpl implements FollowersStats {
 
     @Override
     public Future<Integer> followersCountBy(int id, int depth, Predicate<UserInfo> predicate) {
-        return CompletableFuture.supplyAsync(() -> {
-            CompletableFuture<Integer> total = CompletableFuture.completedFuture(0);
-            Search initialSearch = new Search(id, depth, predicate, network);
-            Queue<Search> pendingSearches = new LinkedList<>(singleton(initialSearch));
-            while (!pendingSearches.isEmpty()) {
-                Search search = pendingSearches.remove();
-                Search.Result result = search.execute();
-                pendingSearches.addAll(result.subSearches.join());
-                total = result.isFilterSatisfied.thenCombine(total, (satisfied, count) -> satisfied ? count + 1 : count);
-            }
-            return total.join();
-        });
+        return search(new Search(id, depth, predicate, network));
+    }
+
+    private CompletableFuture<Integer> search(Search search) {
+        Search.Result result = search.execute();
+        CompletableFuture<Integer> searchCount = result.isFilterSatisfied.thenApply(satisfied -> satisfied ? 1 : 0);
+        CompletableFuture<Integer> subSearchesCount = result.subSearches.thenCompose(subSearches -> subSearches.stream()
+                .map(this::search)
+                .reduce((a, b) -> a.thenCombine(b, Integer::sum))
+                .orElse(CompletableFuture.completedFuture(0))
+        );
+        return searchCount.thenCombine(subSearchesCount, Integer::sum);
     }
 }
