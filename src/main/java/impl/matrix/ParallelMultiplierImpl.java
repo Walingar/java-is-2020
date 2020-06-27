07c027f63770892249471404ca1196bd97aa5d51
lennuz17 <lennuz17@gmail.com>
2020-06-27 21:21:02 +0300

    fix

--- src/main/java/impl/matrix/ParallelMultiplierImpl.java
+++ src/main/java/impl/matrix/ParallelMultiplierImpl.java
@@ -14,13 +14,14 @@ public class ParallelMultiplierImpl implements ParallelMultiplier {
     public double[][] mul(double[][] a, double[][] b) {
         Thread[] threads = new Thread[maxThreads];
         int aRows = a.length;
+        int aColumns = a[0].length;
         int bRows = b.length;
         int bColumns = bRows == 0 ? 0 : b[0].length;
-        int partSize = aRows / maxThreads;
-        int residue = aRows % maxThreads;
+        int resultElementsCount = aRows * bColumns;
+        int partSize = resultElementsCount / maxThreads;
+        int residue = resultElementsCount % maxThreads;
         double[][] result = new double[aRows][bColumns];
 
-       // int cElementsCount = aRowsCount * bColumnsCount;
 
         for (int currentThread = 0; currentThread < maxThreads; currentThread++) {
             int start = currentThread * partSize;
