67ee84a28c76606019d8ef60ff786acc7a5f1dd3
vodanhst <nguyenhoanghuanst1993@gmail.com>
2020-06-28 17:09:33 +0300

    huan_task2_newrefactored

--- src/main/java/impl/expression/ExpressionParser.java
+++ src/main/java/impl/expression/ExpressionParser.java
@@ -3,7 +3,6 @@ package impl.expression;
 import api.expression.ParseException;
 
 public class ExpressionParser implements api.expression.ExpressionParser {
-
     @Override
     public int parse(String expression) throws ParseException {
 
@@ -11,31 +10,29 @@ public class ExpressionParser implements api.expression.ExpressionParser {
             throw new IllegalArgumentException("expression is null");
         }
 
-        String[] expressionParts = expression.split("[\\t\\n ]+");
-
         int sum = 0;
         int numberSign = 1;
 
-        for (int i = 0; i < expressionParts.length; i++) {
+        for (String expressionPart : expression.split("[\\t\\n ]+")) {
             try {
-                if (expressionParts[i].equals("+")) {
+                if (expressionPart.equals("+")) {
                     numberSign = 1;
                     continue;
                 }
-                if (expressionParts[i].equals("-")) {
+                if (expressionPart.equals("-")) {
                     numberSign = -1;
                     continue;
                 }
-                if (expressionParts[i].isEmpty()) {
+                if (expressionPart.isEmpty()) {
                     continue;
                 }
 
-                int temp = Integer.parseInt(expressionParts[i]);
-                if (Math.abs(temp) >= Integer.MAX_VALUE - Math.abs(sum)) {
+                int currentValue = Integer.parseInt(expressionPart);
+                if (Math.abs(currentValue) >= Integer.MAX_VALUE - Math.abs(sum)) {
                     throw new ArithmeticException("overflow");
                 }
 
-                sum = sum + numberSign * temp;
+                sum = sum + numberSign * currentValue;
 
             } catch (NumberFormatException e) {
                 throw new ParseException("long type");
--- src/main/java/impl/expression/ExpressionParserFactory.java
+++ src/main/java/impl/expression/ExpressionParserFactory.java
@@ -1,50 +1,7 @@
 package impl.expression;
 
-import api.expression.ExpressionParser;
-import api.expression.ParseException;
-
-
-
 public class ExpressionParserFactory {
     public static ExpressionParser getInstance() {
-        ExpressionParser expressionParser = new ExpressionParser(){
-
-            @Override
-            public int parse(String expression) throws ParseException {
-                if(expression == null){
-                    throw new IllegalArgumentException("expression is null");
-                }
-                String[] decima = expression.split("[\\t\\n ]+");
-                int sum = 0;
-                int k = 1;
-                for (int i = 0; i<decima.length; i++){
-                    try{
-                        if (decima[i].equals("+")){
-                            k = 1;
-                            continue;
-                        }
-                        if (decima[i].equals("-")){
-                            k = -1;
-                            continue;
-                        }
-                        if (decima[i].isEmpty()){
-                            continue;
-                        }
-
-                        int temp =  Integer.parseInt(decima[i]);
-                        if (Math.abs(temp) >= Math.abs(Integer.MAX_VALUE - sum)){
-                            throw new ArithmeticException("overflow");
-                        }
-                        sum = sum + k*temp;
-
-                    } catch (NumberFormatException e){
-                        throw new ParseException("long type");
-                    }
-
-                }
-                return sum;
-            }
-        };
-        return expressionParser;
+        return new ExpressionParser();
     }
 }
