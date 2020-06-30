b176d15577c31aa2dbb806ce4f6f00fc32c2b6c8
Dmitry <DiamindFaceit@yandex.ru>
2020-06-30 17:52:51 +0300

    [HW_2] remake expression parser

--- src/main/java/api/expression/exception/ParseException.java
+++ src/main/java/api/expression/exception/ParseException.java
@@ -5,4 +5,8 @@ public class ParseException extends Exception {
   public ParseException(String message, Throwable cause) {
     super(message, cause);
   }
+
+  public ParseException(String message) {
+    super(message);
+  }
 }
--- src/main/java/api/expression/impl/ExpressionParserImpl.java
+++ src/main/java/api/expression/impl/ExpressionParserImpl.java
@@ -7,38 +7,63 @@ import java.util.Objects;
 public class ExpressionParserImpl implements ExpressionParser {
 
   private int expressionValue = 0;
+  private int sign = 1;
+  private int currentNumber = 0;
 
   @Override
   public int parse(String expression) throws ParseException {
-    expressionValue = 0;
+    setUp();
 
     if (Objects.isNull(expression)) {
       throw new IllegalArgumentException("Expression must not be null!");
     }
 
-    for (String expressionPart : parseExpression(expression)) {
-      if (expressionPart.isBlank()) {
+    for (char expressionSymbol : expression.toCharArray()) {
+      if (Character.isDigit(expressionSymbol)) {
+        addPartOfNumber(expressionSymbol);
         continue;
       }
 
-      updateExpressionValue(expressionPart);
+      if (isSign(expressionSymbol)) {
+        updateExpressionValue();
+        changeSign(expressionSymbol);
+        continue;
+      }
+
+      if (!Character.isWhitespace(expressionSymbol)) {
+        throw new ParseException("Not a valid expression symbol found!");
+      }
     }
 
+    updateExpressionValue();
     return expressionValue;
   }
 
-  private void updateExpressionValue(String number) throws ParseException {
-    try {
-      expressionValue = Math.addExact(expressionValue, Integer.parseInt(number));
-    } catch (NumberFormatException exception) {
-      throw new ParseException(exception.getMessage(), exception);
-    }
+  private void setUp() {
+    expressionValue = 0;
+    currentNumber = 0;
+    sign = 1;
   }
 
-  private String[] parseExpression(String expression) {
-    return expression
-        .replaceAll("\\s+", "")
-        .replace("-", "+-")
-        .split("\\+");
+  private void changeSign(char expressionSymbol) {
+    sign = (expressionSymbol == '+') ? 1 : -1;
+  }
+
+  private boolean isSign(char expressionSymbol) {
+    return expressionSymbol == '+' || expressionSymbol == '-';
+  }
+
+  private void updateExpressionValue() {
+    expressionValue = Math.addExact(expressionValue, sign * currentNumber);
+    currentNumber = 0;
+  }
+
+  private void addPartOfNumber(char symbol) throws ParseException {
+    try {
+      currentNumber = Math
+          .addExact(Math.multiplyExact(currentNumber, 10), Character.getNumericValue(symbol));
+    } catch (ArithmeticException exception) {
+      throw new ParseException(exception.getMessage(), exception);
+    }
   }
 }
